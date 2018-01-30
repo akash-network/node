9632499a7d67ea875b4478fceec6998bf3ca57a5
aastein <aaronstein12@gmail.com>
2018-01-30 13:27:29 -0800

    contract updates

--- ethereum/src/photon.sol
+++ ethereum/src/photon.sol
@@ -104,11 +104,92 @@ contract Parameterized is Maintainable {
     }
 }
 
+ contract Client is Ownable, Parameterized, Matchable, Cancelable, Payable, Delinquent {
+
+    Provider public matchedProvider;
+    uint public matchStartTime;
+    uint public totalBilled;
+    uint public unsettledBalance;
+    uint public maxUnsettledBalance;
+    string public manifest;
+
+    // Constructor
+    function Client(address maintainer, uint ram, uint cpu, uint rate, uint _minimumBalanace, uint cancelFee, uint _maxUnsettledBalance, string _manifest) public payable Parameterized(ram, cpu, rate, _minimumBalanace, cancelFee) Maintainable(maintainer) {
+        maxUnsettledBalance = _maxUnsettledBalance;
+        manifest = _manifest;
+    }
+
+    // match with provider
+    function matchProvider(address providerAddress) external onlyOwner notCanceled notMatched returns (bool) {
+        matchedProvider = Provider(providerAddress);
+        matchStartTime = now;
+        matched = true;
+        return matched;
+    }
+
+    // send the value of the outstanding bill to the provider
+    function bill() public notCanceled {
+         uint payment;
+        // pay required amount since last payment time
+        // get the required payment by total payment - expected payment
+        unsettledBalance = (now - matchStartTime) * rate - totalBilled;
+        // pay the provider
+        if (this.balance > unsettledBalance) {
+            payment = unsettledBalance;
+        } else {
+            payment = this.balance;
+        }
+        // update the total amount billed and unsetted
+        totalBilled += payment;
+        unsettledBalance -= payment;
+        // send payment
+        matchedProvider.transfer(payment);
+
+        // if unselted balance is greater than maxUnsettledBalance
+        // let provider cancel it by setting delinquent to true
+        if (unsettledBalance > maxUnsettledBalance) {
+            delinquent = true;
+        }
+    }
+
+    // allow maintainer to withdrawal ETH
+    function withdrawal(uint amount) public onlyMaintainer notDelinquent {
+        maintainer.transfer(amount);
+    }
+
+    // reset contract fields
+    function reset() private {
+        matched = false;
+        canceled = true;
+        matchedProvider = Provider(0x0);
+    }
+
+    // reset contract fields. Prevents payments.
+    function cancel() public onlyMaintainer notCanceled {
+        matchedProvider.clientCancel();
+        reset();
+    }
+
+    // lets the provider cancel the contract for deliquent payment
+    // leave delinquent true so there is a perminent record of clients bad behavior
+    function providerCancel() public isDelinquent {
+        // only matchedProvider should be able to call this
+        require(msg.sender == address(matchedProvider));
+        reset();
+        matchedProvider.transfer(this.balance);
+    }
+
+    // makes canceled false. contract can be rematched to a provider.
+    function uncancel() public onlyMaintainer isCanceled notDelinquent {
+        canceled = false;
+    }
+
+    function () public payable {}
+}
+
 contract Provider is Ownable, Parameterized, Matchable, Cancelable, Payable, BadActor {
 
-    // address of matched client
     Client public matchedClient;
-    // Provider's public endpoint for Manifest distribution
     string public networkAddress;
 
     // Constructor
@@ -117,26 +198,45 @@ contract Provider is Ownable, Parameterized, Matchable, Cancelable, Payable, Bad
     }
 
     // confirm and declare that a valid matching contract is at an address
-    function matchClient(address clientAddress) public onlyOwner notCanceled notMatched returns (bool) {
+    function matchClient(address clientAddress) external onlyOwner notCanceled notMatched returns (bool) {
         // load client contract
         Client client = Client(clientAddress);
         // make sure resources match
-        require(client.cpu() <= cpu && client.ram() <= ram && client.rate() >= rate && client.minimumBalanace() >= minimumBalanace && client.cancelFee() <= cancelFee);
+        require(client.cpu() <= cpu
+        && client.ram() <= ram
+        && client.rate() >= rate
+        && client.minimumBalanace() >= minimumBalanace
+        && client.cancelFee() <= cancelFee);
         matchedClient = client;
         matched = true;
         return matched;
     }
 
-    // reset contract fields. Charge fee if early cancelation. Sends contract balance to maintainer
-    function cancel() public notCanceled onlyMaintainer {
+    // reset contract fields
+    function reset() private {
         matched = false;
         canceled = true;
+        matchedClient = Client(0x0);
+    }
 
-        // if the matchedClients balance has falled below the minium balance do not charge an early cancellation fee
-        if (matchedClient.balance >= matchedClient.minimumBalanace()) {
+    // maintainer cancels contract and incurrs a fee
+    function earlyCancel() public onlyMaintainer notCanceled  {
+        reset();
+        // one last bill to the client
+        matchedClient.bill();
+        // if the matchedClient is not delinquent charge an early cancellation fee
+        if (!matchedClient.delinquent()) {
             matchedClient.transfer(cancelFee);
         }
-        maintainer.transfer(this.balance);
+    }
+
+    // client cancels contract
+    function clientCancel() external notCanceled {
+        // only matchedClient should be able to call this
+        require(msg.sender == address(matchedClient));
+        // one last bill to the client
+        matchedClient.bill();
+        reset();
     }
 
     // makes canceled false. contract can be rematched to a provider.
@@ -144,90 +244,30 @@ contract Provider is Ownable, Parameterized, Matchable, Cancelable, Payable, Bad
         canceled = false;
     }
 
-    // send funds to contract maintainer
+    // send maximum allowable funds to contract maintainer
     function withdrawal() public {
-        uint amount = this.balance - cancelFee;
-        // ensure contract always can pay the early cancel fee
+         uint amount = this.balance;
+        if (!canceled) {
+            // ensure contract always can pay the early cancel fee when not canceled
+            amount = amount - cancelFee;
+        }
         require(amount > 0);
         maintainer.transfer(amount);
-    }
-}
-
- contract Client is Ownable, Parameterized, Matchable, Cancelable, Payable, Delinquent {
-
-    Provider public matchedProvider;
-    uint public matchStartTime;
-    uint public totalBilled;
-    uint public unsettledBalance;
-    uint public maxUnsettledBalance;
-
-    // Constructor
-    function Client(address maintainer, uint ram, uint cpu, uint rate, uint _minimumBalanace, uint cancelFee, uint _maxUnsettledBalance) public payable Parameterized(ram, cpu, rate, _minimumBalanace, cancelFee) Maintainable(maintainer) {
-        maxUnsettledBalance = _maxUnsettledBalance;
-    }
-
-    // match with provider
-    function matchProvider(address providerAddress) public onlyOwner notCanceled notMatched returns (bool) {
-        matchedProvider = Provider(providerAddress);
-        matchStartTime = now;
-        matched = true;
-        return matched;
-    }
-
-    // create a bill for the client.
-    function setBill() public {
-        // pay required amount since last payment time
-        // get the required payment by total payment - expected payment
-        uint expectedBilled = (now - matchStartTime) * rate;
-        unsettledBalance = expectedBilled - totalBilled;
-    }
-
-    // send the value of the outstanding bill to the provider
-    function bill() public notCanceled {
-        // pay the provider
-        uint payment;
-        if (this.balance > unsettledBalance) {
-            payment = unsettledBalance;
-        } else {
-           payment = this.balance;
-        }
-        matchedProvider.transfer(payment);
-        // update the total amount billed and unsetted
-        totalBilled += payment;
-        unsettledBalance -= payment;
-
-        // if unselted balance is > N hour value of contract let provider cancel it
-        if (unsettledBalance > maxUnsettledBalance) {
-            delinquent = true;
-        }
-    }
-
-    // reset contract fields. Prevents payments. Sends contract balance to maintainer
-    function cancel() public onlyMaintainer notCanceled notDelinquent {
-        matched = false;
-        canceled = true;
-        maintainer.transfer(this.balance);
-    }
-
-    // lets the provider cancel the contract for deliquent payment
-    // leave delinquent true so there is a perminent record of clients bad behavior
-    function providerCancel() public isDelinquent {
-        // only matchedProvider should be able to call this
-        require(msg.sender == address(matchedProvider));
-        matched = false;
-        canceled = true;
-        matchedProvider.transfer(this.balance);
-    }
-
-    // makes canceled false. contract can be rematched to a provider.
-    function uncancel() public isCanceled notDelinquent{
-        canceled = false;
     }
 }
 
 contract Master is Maintainable {
 
     function Master() public Maintainable(msg.sender) {}
+
+     // call to put an ask for a service on the network
+    function deployClient(uint _ram, uint _cpu, uint _rate, uint _minimumBalanace, uint _cancelFee, uint _maxUnsettledBalance, string _manifest) public payable returns (Client) {
+        // ensure client contract has minimum balance
+        require(msg.value >= _minimumBalanace);
+        Client client = new Client(msg.sender, _ram, _cpu, _rate, _minimumBalanace, _cancelFee, _maxUnsettledBalance, _manifest);
+        client.transfer(msg.value);
+        return client;
+    }
 
     // call to put a bid for a service on the network
     function deployProvider(uint _ram, uint _cpu, uint _rate, uint _minimumBalanace, uint _cancelFee, string _networkAddress) public payable returns (Provider) {
@@ -238,14 +278,6 @@ contract Master is Maintainable {
         return provider;
     }
 
-    // call to put an ask for a service on the network
-    function deployClient(uint _ram, uint _cpu, uint _rate, uint _minimumBalanace, uint _cancelFee, uint _maxUnsettledBalance) public payable returns (Client) {
-        // ensure client contract has minimum balance
-        require(msg.value >= _minimumBalanace);
-        Client client = new Client(msg.sender, _ram, _cpu, _rate, _minimumBalanace, _cancelFee, _maxUnsettledBalance);
-        client.transfer(msg.value);
-        return client;
-    }
 
     // try to maych a client and a provider
     // this is onlyMaintainer because the matching must be done in a fair way
