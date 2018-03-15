ad08a1eff2c36259c1986e7364ebb9cc7ea28fa4
aastein <aaron@ovrclk.com>
2018-03-15 16:39:53 -0700

    remove prints

--- cmd/akashd/start_test.go
+++ cmd/akashd/start_test.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"os"
 	"testing"
 	"time"
@@ -13,85 +11,70 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func Test_Start_Fail(t *testing.T) {
+	emmptydir := testutil.TempDir(t)
+	defer os.RemoveAll(emmptydir)
+	args := []string{startCommand().Name(), "-d", emmptydir}
+	base := baseCommand()
+	base.AddCommand(startCommand())
+	base.SetArgs(args)
+	require.Error(t, base.Execute())
+}
+
 func Test_Start(t *testing.T) {
 
 	basedir := testutil.TempDir(t)
 	defer os.RemoveAll(basedir)
 
-	{
-		emmptydir := testutil.TempDir(t)
-		defer os.RemoveAll(emmptydir)
-		args := []string{startCommand().Name(), "-d", emmptydir}
-		base := baseCommand()
-		base.AddCommand(startCommand())
-		base.SetArgs(args)
-		println("////////// start ignore expected error //////////")
-		require.Error(t, base.Execute())
-		println("////////// end ignore expected error //////////")
-	}
+	// init genesis data
+	genesispath := basedir + "/config/genesis.json"
+	viper.Reset()
 
-	{
-		// init genesis data
-		genesispath := basedir + "/config/genesis.json"
-		viper.Reset()
+	state := testutil.NewState(t, nil)
+	address, _ := testutil.CreateAccount(t, state)
+	addr := address.Address.EncodeString()
 
-		state := testutil.NewState(t, nil)
-		address, _ := testutil.CreateAccount(t, state)
-		addr := address.Address.EncodeString()
+	args := []string{initCommand().Name(), addr, "-d", basedir}
 
-		args := []string{initCommand().Name(), addr, "-d", basedir}
+	base := baseCommand()
+	base.AddCommand(initCommand())
+	base.SetArgs(args)
+	require.NoError(t, base.Execute())
 
-		ls(basedir + "/config")
+	tmgenesis, err := node.TMGenesisFromFile(genesispath)
+	require.NoError(t, err)
+	_, err = node.GenesisFromTMGenesis(tmgenesis)
+	require.NoError(t, err)
 
-		base := baseCommand()
-		base.AddCommand(initCommand())
-		base.SetArgs(args)
-		require.NoError(t, base.Execute())
+	// run node
+	startargs := []string{startCommand().Name(), "-d", basedir}
+	startbase := baseCommand()
+	startbase.AddCommand(startCommand())
+	startbase.SetArgs(startargs)
 
-		ls(basedir + "/config")
+	errchan := make(chan error)
+	quit := make(chan bool)
+	// defer close(errchan)
+	defer close(quit)
 
-		tmgenesis, err := node.TMGenesisFromFile(genesispath)
-		require.NoError(t, err)
-		_, err = node.GenesisFromTMGenesis(tmgenesis)
-		require.NoError(t, err)
+	time.AfterFunc(3*time.Second, func() { quit <- true })
 
-		ls(basedir + "/config")
-
-		// run node
-		startargs := []string{startCommand().Name(), "-d", basedir}
-		startbase := baseCommand()
-		startbase.AddCommand(startCommand())
-		startbase.SetArgs(startargs)
-
-		ls(basedir + "/config")
-
-		errchan := make(chan error)
-		quit := make(chan bool)
-		defer close(errchan)
-		defer close(quit)
-
-		time.AfterFunc(3*time.Second, func() { quit <- true })
-
-		started := false
-		run := true
-		for run {
-			if started == false {
-				go func() {
-					ls(basedir + "/config")
-					errchan <- startbase.Execute()
-					ls(basedir + "/config")
-				}()
-				started = true
-			}
-			if <-quit {
-				break
-			}
+	started := false
+	run := true
+	for run {
+		if started == false {
+			go func() {
+				errchan <- startbase.Execute()
+			}()
+			started = true
 		}
-
-		ls(basedir + "/config")
-		err = emptyErrChannel(errchan)
-		require.NoError(t, err)
+		if <-quit {
+			break
+		}
 	}
+
+	err = emptyErrChannel(errchan)
+	require.NoError(t, err)
 }
 
 func emptyErrChannel(ch chan error) error {
@@ -100,17 +83,5 @@ func emptyErrChannel(ch chan error) error {
 		return x
 	default:
 		return nil
-	}
-}
-
-func ls(dir string) {
-	files, err := ioutil.ReadDir(dir)
-	if err != nil {
-		println("err:", err.Error())
-	}
-
-	println("Files in ", dir)
-	for _, f := range files {
-		fmt.Println(f.Name())
 	}
 }
