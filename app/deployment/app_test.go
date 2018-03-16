ddce4d4bf9922b96cb11b6dfd5f7d9ada9301705
aastein <aaron@ovrclk.com>
2018-03-16 16:27:06 -0700

    tests

--- app/deployment/app_test.go
+++ app/deployment/app_test.go
@@ -1,10 +1,12 @@
 package deployment_test
 
 import (
+	"encoding/hex"
 	"fmt"
 	"testing"
 
 	"github.com/ovrclk/akash/app/deployment"
+	apptypes "github.com/ovrclk/akash/app/types"
 	pstate "github.com/ovrclk/akash/state"
 	"github.com/ovrclk/akash/testutil"
 	"github.com/ovrclk/akash/types"
@@ -59,6 +61,32 @@ func TestValidTx(t *testing.T) {
 	}
 
 	{
+		resp := app.Query(tmtypes.RequestQuery{Path: pstate.DeploymentPath})
+		assert.Empty(t, resp.Log)
+		require.True(t, resp.IsOK())
+	}
+
+	{
+		resp := app.Query(tmtypes.RequestQuery{Path: pstate.DeploymentPath + hex.EncodeToString(pstate.DeploymentGroupID(depl.Address, 1))})
+		assert.NotEmpty(t, resp.Log)
+		require.False(t, resp.IsOK())
+	}
+
+	{
+		path := pstate.DeploymentGroupPath + hex.EncodeToString(pstate.DeploymentGroupID(depl.Address, 1))
+		resp := app.Query(tmtypes.RequestQuery{Path: path})
+		assert.Empty(t, resp.Log)
+		require.True(t, resp.IsOK())
+	}
+
+	{
+		path := pstate.DeploymentGroupPath + hex.EncodeToString(pstate.DeploymentGroupID(depl.Address, 0))
+		resp := app.Query(tmtypes.RequestQuery{Path: path})
+		assert.NotEmpty(t, resp.Log)
+		require.False(t, resp.IsOK())
+	}
+
+	{
 		groups, err := state.DeploymentGroup().ForDeployment(depl.Address)
 		require.NoError(t, err)
 		require.Len(t, groups, 1)
@@ -66,4 +94,18 @@ func TestValidTx(t *testing.T) {
 		assert.Equal(t, groups[0].Requirements, depl.Groups[0].Requirements)
 		assert.Equal(t, groups[0].Resources, depl.Groups[0].Resources)
 	}
+}
+
+func TestTx_BadTxType(t *testing.T) {
+	state_ := testutil.NewState(t, nil)
+	app, err := deployment.NewApp(state_, testutil.Logger())
+	require.NoError(t, err)
+	account, key := testutil.CreateAccount(t, state_)
+	tx := testutil.ProviderTx(account, &key, 10)
+	ctx := apptypes.NewContext(tx)
+	assert.False(t, app.AcceptTx(ctx, tx.Payload.Payload))
+	cresp := app.CheckTx(ctx, tx.Payload.Payload)
+	assert.False(t, cresp.IsOK())
+	dresp := app.DeliverTx(ctx, tx.Payload.Payload)
+	assert.False(t, dresp.IsOK())
 }
