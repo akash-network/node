f600845714df1aa040f64cef9ff4138e7a07c5c8
aastein <aaron@ovrclk.com>
2018-03-16 13:12:05 -0700

    connection tests

--- cmd/akash/connection_test.go
+++ cmd/akash/connection_test.go
@@ -31,3 +31,49 @@ func TestProviderRun_NoNode(t *testing.T) {
 	assert.Error(t, err)
 	assert.Contains(t, err.Error(), "connection refused")
 }
+
+func TestDeployment_NoNode(t *testing.T) {
+	path := "deployment.yaml"
+	info, _ := testutil.NewNamedKey(t)
+	args := []string{deploymentCommand().Name(), path, "-k", info.Name}
+
+	base := baseCommand()
+	base.AddCommand(deploymentCommand())
+	base.SetArgs(args)
+	err := base.Execute()
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "connection refused")
+}
+
+func TestMarketplace_NoNode(t *testing.T) {
+	args := []string{marketplaceCommand().Name()}
+	base := baseCommand()
+	base.AddCommand(marketplaceCommand())
+	base.SetArgs(args)
+	err := base.Execute()
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "connection refused")
+}
+
+func TestSend_NoNode(t *testing.T) {
+	from, _ := testutil.NewNamedKey(t)
+	to, _ := testutil.NewNamedKey(t)
+	amount := "1"
+	args := []string{sendCommand().Name(), amount, to.Address.String(), "-k", from.Name}
+	base := baseCommand()
+	base.AddCommand(sendCommand())
+	base.SetArgs(args)
+	err := base.Execute()
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "connection refused")
+}
+
+func TestStatus_NoNode(t *testing.T) {
+	args := []string{statusCommand().Name()}
+	base := baseCommand()
+	base.AddCommand(statusCommand())
+	base.SetArgs(args)
+	err := base.Execute()
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "connection refused")
+}
