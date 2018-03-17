1fba795f32df1eed35d47a4f28f8407ab2d566ca
aastein <aaron@ovrclk.com>
2018-03-16 17:20:36 -0700

    no node query tests

---
+++ cmd/akash/query_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/ovrclk/akash/cmd/akash/query"
+	"github.com/ovrclk/akash/testutil"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAccountQuery_NoNode(t *testing.T) {
+	info, _ := testutil.NewNamedKey(t)
+	args := []string{query.QueryCommand().Name(), "account", info.Address.String()}
+	base := baseCommand()
+	base.AddCommand(query.QueryCommand())
+	base.SetArgs(args)
+	err := base.Execute()
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "connection refused")
+}
+
+func TestDeploymentQuery_NoNode(t *testing.T) {
+	info, _ := testutil.NewNamedKey(t)
+	args := []string{query.QueryCommand().Name(), "deployment", info.Address.String()}
+	base := baseCommand()
+	base.AddCommand(query.QueryCommand())
+	base.SetArgs(args)
+	err := base.Execute()
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "connection refused")
+}
+
+func TestOrderQuery_NoNode(t *testing.T) {
+	info, _ := testutil.NewNamedKey(t)
+	args := []string{query.QueryCommand().Name(), "order", info.Address.String()}
+	base := baseCommand()
+	base.AddCommand(query.QueryCommand())
+	base.SetArgs(args)
+	err := base.Execute()
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "connection refused")
+}
+
+func TestProviderQuery_NoNode(t *testing.T) {
+	info, _ := testutil.NewNamedKey(t)
+	args := []string{query.QueryCommand().Name(), "provider", info.Address.String()}
+	base := baseCommand()
+	base.AddCommand(query.QueryCommand())
+	base.SetArgs(args)
+	err := base.Execute()
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "connection refused")
+}
