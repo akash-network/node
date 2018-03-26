09e3fefda619fdb64c055435fcd9100aeaeba83e
Adam Bozanich <adam.boz@gmail.com>
2018-03-26 16:56:50 -0700

    checkpt

--- _integration/cmp/account.go
+++ _integration/cmp/account.go
@@ -1,26 +1,31 @@
 package cmp
 
 import (
+	"strconv"
+
+	"github.com/ovrclk/akash/_integration/js"
 	"github.com/ovrclk/gestalt"
 	g "github.com/ovrclk/gestalt/builder"
 )
 
 func AccountBalance(key key, amount int) gestalt.Component {
-	// check account balance
-	return g.Group("account-balance")
+	return Akash("query", "account", key.addr.Var()).
+		FN(js.PathEQInt(amount, "balance"))
 }
 
 func AccountSendTo(from key, to key, amount int) gestalt.Component {
-	// send `amount` from `from` to `to`
-	return g.Group("account-send")
+	return Akash("send", strconv.Itoa(amount), to.addr.Var(), "-k", from.name.Name())
 }
 
 func GroupAccountSend(key key) gestalt.Component {
+	start := 1000000000
+	amount := 100
 	other := newKey("other")
 	return g.Group("account-send").
 		Run(GroupKeyCreate(other)).
-		Run(AccountBalance(key, 10000)).
-		Run(AccountSendTo(key, other, 100)).
-		Run(AccountBalance(key, 10000-100)).
-		Run(AccountBalance(other, 100))
+		Run(AccountBalance(key, start)).
+		Run(AccountSendTo(key, other, amount)).
+		Run(g.Retry(5).
+			Run(AccountBalance(key, start-amount))).
+		Run(AccountBalance(other, amount))
 }
---
+++ _integration/js/obj.go
@@ -0,0 +1,30 @@
+package js
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+
+	"github.com/buger/jsonparser"
+	"github.com/ovrclk/gestalt"
+	"github.com/ovrclk/gestalt/exec"
+)
+
+func PathEQInt(expect int, path ...string) exec.CmdFn {
+	return func(b *bufio.Reader, e gestalt.Evaluator) error {
+		buf := new(bytes.Buffer)
+		if _, err := b.WriteTo(buf); err != nil {
+			return err
+		}
+
+		val, err := jsonparser.GetInt(buf.Bytes(), path...)
+		if err != nil {
+			return err
+		}
+
+		if val != int64(expect) {
+			return fmt.Errorf("received %v expected %v", val, expect)
+		}
+		return nil
+	}
+}
