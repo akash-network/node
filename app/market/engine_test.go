e5c855b47431da891be324da15fa83c10d0e3410
aastein <aaron@ovrclk.com>
2018-03-27 12:48:27 -0700

    [ci skip] checkpt

--- app/market/engine_test.go
+++ app/market/engine_test.go
@@ -16,15 +16,26 @@ func TestEngine_All(t *testing.T) {
 
 	tenant, _ := testutil.CreateAccount(t, state_)
 
+	pacc, _ := testutil.CreateAccount(t, state_)
+	provider := testutil.Provider(pacc.Address, 0)
+	require.NoError(t, state_.Provider().Save(provider))
+
 	deployment := testutil.Deployment(tenant.Address, tenant.Nonce)
 	groups := testutil.DeploymentGroups(deployment.Address, tenant.Nonce)
 	require.NoError(t, state_.Deployment().Save(deployment))
 
+	state_, tx := testOrder(t, state_, tenant, deployment, groups)
+	state_ = testLease(t, state_, provider, deployment, groups, tx)
+	state_ = testBilling(t, state_, tenant, provider)
+
+}
+
+func testOrder(t *testing.T, state state.State, tenant *types.Account, deployment *types.Deployment, groups *types.DeploymentGroups) (state.State, *types.TxCreateOrder) {
 	for idx := range groups.GetItems() {
-		require.NoError(t, state_.DeploymentGroup().Save(groups.GetItems()[idx]))
+		require.NoError(t, state.DeploymentGroup().Save(groups.GetItems()[idx]))
 	}
 
-	txs, err := market.NewEngine(testutil.Logger()).Run(state_)
+	txs, err := market.NewEngine(testutil.Logger()).Run(state)
 	require.NoError(t, err)
 
 	require.Len(t, txs, 1)
@@ -35,45 +46,51 @@ func TestEngine_All(t *testing.T) {
 	require.Equal(t, deployment.Address, tx.Order.Deployment)
 	require.Equal(t, groups.GetItems()[0].Seq, tx.Order.GetGroup())
 	require.Equal(t, types.Order_OPEN, tx.Order.GetState())
-	require.NoError(t, state_.Order().Save(tx.Order))
+	require.NoError(t, state.Order().Save(tx.Order))
 
-	pacc, _ := testutil.CreateAccount(t, state_)
-	provider := testutil.Provider(pacc.Address, 0)
-	require.NoError(t, state_.Provider().Save(provider))
+	return state, tx
+}
 
+func testLease(t *testing.T, state state.State, provider *types.Provider, deployment *types.Deployment, groups *types.DeploymentGroups, tx *types.TxCreateOrder) state.State {
 	fulfillment := testutil.Fulfillment(provider.Address, deployment.Address, tx.Order.GetGroup(), tx.Order.GetOrder(), 1)
-	require.NoError(t, state_.Fulfillment().Save(fulfillment))
+	require.NoError(t, state.Fulfillment().Save(fulfillment))
 
 	for i := int64(0); i <= groups.GetItems()[0].OrderTTL; i++ {
-		state_.Commit()
+		state.Commit()
 	}
 
-	txs, err = market.NewEngine(testutil.Logger()).Run(state_)
+	txs, err := market.NewEngine(testutil.Logger()).Run(state)
 	require.NoError(t, err)
 	require.Len(t, txs, 1)
 
 	leaseTx, ok := txs[0].(*types.TxCreateLease)
 	require.True(t, ok)
-	require.NoError(t, state_.Lease().Save(leaseTx.GetLease()))
-	require.NoError(t, state_.Lease().Save(leaseTx.GetLease()))
+	require.NoError(t, state.Lease().Save(leaseTx.GetLease()))
+	require.NoError(t, state.Lease().Save(leaseTx.GetLease()))
 
 	matchedOrder := tx.GetOrder()
 	matchedOrder.State = types.Order_MATCHED
-	require.NoError(t, state_.Order().Save(matchedOrder))
+	require.NoError(t, state.Order().Save(matchedOrder))
 
-	iTenBal := getBalance(t, state_, tenant.Address)
-	iProBal := getBalance(t, state_, provider.Owner)
+	return state
+}
+
+func testBilling(t *testing.T, state state.State, tenant *types.Account, provider *types.Provider) state.State {
+	iTenBal := getBalance(t, state, tenant.Address)
+	iProBal := getBalance(t, state, provider.Owner)
 	require.NotZero(t, iTenBal)
 	require.NotZero(t, iProBal)
 
-	txs, err = market.NewEngine(testutil.Logger()).Run(state_)
+	txs, err := market.NewEngine(testutil.Logger()).Run(state)
 	require.NoError(t, err)
 	require.Len(t, txs, 0)
 
-	fTenBal := getBalance(t, state_, tenant.Address)
-	fProBal := getBalance(t, state_, provider.Owner)
+	fTenBal := getBalance(t, state, tenant.Address)
+	fProBal := getBalance(t, state, provider.Owner)
 	require.Equal(t, iTenBal-1, fTenBal)
 	require.Equal(t, iProBal+1, fProBal)
+
+	return state
 }
 
 func getBalance(t *testing.T, state state.State, address base.Bytes) uint64 {
