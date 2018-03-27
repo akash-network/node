d2196068c6076e210d57a523dac69dc0d175ceda
Adam Bozanich <adam.boz@gmail.com>
2018-03-27 15:39:02 -0700

    testing: run skipped tests nightly

--- testutil/shrug.go
+++ testutil/shrug.go
@@ -2,11 +2,14 @@ package testutil
 
 import (
 	"fmt"
+	"os"
 	"testing"
 )
 
 const shrug = `¯\_(ツ)_/¯`
 
 func Shrug(t *testing.T, issue int) {
-	t.Skip(fmt.Sprintf("%v - https://github.com/ovrclk/akash/issues/%d", shrug, issue))
+	if os.Getenv("TEST_UNSKIP") == "" {
+		t.Skip(fmt.Sprintf("%v - https://github.com/ovrclk/akash/issues/%d", shrug, issue))
+	}
 }
