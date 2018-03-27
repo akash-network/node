9af4c01b11b85d70f082e3b48cb16a98f423995d
Adam Bozanich <adam.boz@gmail.com>
2018-03-27 00:26:09 -0700

    [ci skip] checkpt

--- _integration/cmp/deployment.go
+++ _integration/cmp/deployment.go
@@ -2,20 +2,44 @@ package cmp
 
 import (
 	"github.com/ovrclk/akash/_integration/js"
+	"github.com/ovrclk/akash/types"
 	"github.com/ovrclk/gestalt"
 	g "github.com/ovrclk/gestalt/builder"
 	"github.com/ovrclk/gestalt/vars"
 )
 
 func DeployCreate(key vars.Ref, daddr vars.Ref) gestalt.Component {
-	return Akash("deployment", "create", "unused.yml", "-k", key.Name()).
-		FN(g.Capture(daddr.Name())).
+	parse := js.Do(
+		js.Str(daddr.Var(), "address"),
+	)
+
+	return g.Group("deploy-create").
+		Run(
+			Akash("deployment", "create", "unused.yml", "-k", key.Name()).
+				FN(g.Capture(daddr.Name())).
+				WithMeta(g.Export(daddr.Name()))).
+		Run(
+			g.Retry(5).
+				Run(
+					Akash("query", "deployment", daddr.Var()).
+						FN(parse))).
 		WithMeta(g.Export(daddr.Name()))
 }
 
-func DeployQuery(daddr vars.Ref) gestalt.Component {
+func DeployClose(key vars.Ref, daddr vars.Ref) gestalt.Component {
+	return Akash("deployment", "close", daddr.Var(), "-k", key.Name()).
+		WithMeta(g.Require(daddr.Name()))
+}
+
+func DeployQuery(daddr vars.Ref, state types.Deployment_DeploymentState) gestalt.Component {
+
+	parse := js.Do(
+		js.Str(daddr.Var(), "address"),
+		js.Int(int64(state), "state"),
+	)
+
 	return Akash("query", "deployment", daddr.Var()).
-		FN(js.Do(js.Str(daddr.Var(), "address"))).
+		FN(parse).
 		WithMeta(g.Export(daddr.Var()))
 }
 
@@ -32,13 +56,14 @@ func LeaseQuery(daddr vars.Ref) gestalt.Component {
 }
 
 func GroupDeployCreate(key vars.Ref, daddr vars.Ref) gestalt.Component {
-	return g.Group("deploy-create").
+	return g.Group("deployment").
 		Run(DeployCreate(key, daddr)).
-		Run(g.Retry(5).
-			Run(DeployQuery(daddr))).
 		Run(g.Retry(5).
 			Run(OrderQuery(daddr))).
 		Run(g.Retry(10).
 			Run(LeaseQuery(daddr))).
+		Run(DeployClose(key, daddr)).
+		Run(g.Retry(5).
+			Run(DeployQuery(daddr, types.Deployment_CLOSED))).
 		WithMeta(g.Export(daddr.Name()))
 }
