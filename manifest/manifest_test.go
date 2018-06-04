110c6298d9962ce819687ebe5287729a37801d26
aastein <aaron@ovrclk.com>
2018-06-04 11:40:12 -0700

    golint

--- manifest/manifest_test.go
+++ manifest/manifest_test.go
@@ -138,28 +138,36 @@ func TestVerifyDeploymentTennant(t *testing.T) {
 	info, kmgr := testutil.NewNamedKey(t)
 	signer := testutil.Signer(t, kmgr)
 	tenant := info.Address()
-
 	deployment := testutil.Deployment(tenant, 1)
-
 	providerID := testutil.Address(t)
 	provider := testutil.Provider(providerID, 4)
-
 	client := &qmocks.Client{}
 	client.On("Deployment",
 		mock.Anything,
 		[]byte(deployment.Address)).Return(deployment, nil)
-
 	sess := session.New(testutil.Logger(), provider, nil, client)
-
 	mani := &types.Manifest{}
-	mreq := &types.ManifestRequest{
-		Deployment: deployment.Address,
-		Manifest:   mani,
-	}
-
 	mreq, _, err := SignManifest(mani, signer, deployment.Address)
 	require.NoError(t, err)
+	err = verifyDeploymentTennant(mreq, sess, info.Address())
+	assert.NoError(t, err)
+}
 
+func TestVerifyDeploymentTennant_InvalidKey(t *testing.T) {
+	info, kmgr := testutil.NewNamedKey(t)
+	signer := testutil.Signer(t, kmgr)
+	tenant := info.Address()
+	deployment := testutil.Deployment(tenant, 1)
+	providerID := testutil.Address(t)
+	provider := testutil.Provider(providerID, 4)
+	client := &qmocks.Client{}
+	client.On("Deployment",
+		mock.Anything,
+		[]byte(deployment.Address)).Return(deployment, nil)
+	sess := session.New(testutil.Logger(), provider, nil, client)
+	mani := &types.Manifest{}
+	mreq, _, err := SignManifest(mani, signer, deployment.Address)
+	require.NoError(t, err)
 	err = verifyDeploymentTennant(mreq, sess, info.Address())
 	assert.NoError(t, err)
 }
