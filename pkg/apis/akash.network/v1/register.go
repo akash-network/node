b5a351ee6232f329ed317f1c78eb442cb4f189c3
aastein <aaron@ovrclk.com>
2018-06-19 09:07:35 -0700

    checkpoint

---
+++ pkg/apis/akash.network/v1/register.go
@@ -0,0 +1,82 @@
+package v1
+
+import (
+	"reflect"
+
+	apiextv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	apiextcs "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/client-go/rest"
+)
+
+const (
+	CRDPlural   string = "manifests"
+	CRDGroup    string = "akash.network"
+	CRDVersion  string = "v1"
+	FullCRDName string = CRDPlural + "." + CRDGroup
+)
+
+var (
+	// localSchemeBuilder and AddToScheme will stay in k8s.io/kubernetes.
+	SchemeBuilder      runtime.SchemeBuilder
+	localSchemeBuilder = &SchemeBuilder
+	AddToScheme        = localSchemeBuilder.AddToScheme
+)
+
+// Create a  Rest client with the new CRD Schema
+var SchemeGroupVersion = schema.GroupVersion{Group: CRDGroup, Version: CRDVersion}
+
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(SchemeGroupVersion,
+		&ManifestCRD{},
+		&ManifestCRDList{},
+	)
+	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
+	return nil
+}
+
+// Create the CRD resource, ignore error if it already exists
+func CreateCRD(clientset apiextcs.Interface) error {
+	crd := &apiextv1beta1.CustomResourceDefinition{
+		ObjectMeta: metav1.ObjectMeta{Name: FullCRDName},
+		Spec: apiextv1beta1.CustomResourceDefinitionSpec{
+			Group:   CRDGroup,
+			Version: CRDVersion,
+			Scope:   apiextv1beta1.NamespaceScoped,
+			Names: apiextv1beta1.CustomResourceDefinitionNames{
+				Plural: CRDPlural,
+				Kind:   reflect.TypeOf(ManifestCRD{}).Name(),
+			},
+		},
+	}
+
+	_, err := clientset.ApiextensionsV1beta1().CustomResourceDefinitions().Create(crd)
+	if err != nil && apierrors.IsAlreadyExists(err) {
+		return nil
+	}
+	return err
+}
+
+func NewClient(cfg *rest.Config) (*rest.RESTClient, *runtime.Scheme, error) {
+	scheme := runtime.NewScheme()
+	SchemeBuilder := runtime.NewSchemeBuilder(addKnownTypes)
+	if err := SchemeBuilder.AddToScheme(scheme); err != nil {
+		return nil, nil, err
+	}
+	config := *cfg
+	config.GroupVersion = &SchemeGroupVersion
+	config.APIPath = "/apis"
+	config.ContentType = runtime.ContentTypeJSON
+	config.NegotiatedSerializer = serializer.DirectCodecFactory{
+		CodecFactory: serializer.NewCodecFactory(scheme)}
+
+	client, err := rest.RESTClientFor(&config)
+	if err != nil {
+		return nil, nil, err
+	}
+	return client, scheme, nil
+}
---
+++ pkg/apis/akash.network/v1/types.go
@@ -0,0 +1,109 @@
+package v1
+
+import (
+	"github.com/ovrclk/akash/types"
+	"github.com/ovrclk/akash/types/base"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type ManifestCRD struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata"`
+	Spec              ManifestSpec
+	Status            ManifestStatus
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type LeaseID struct {
+	metav1.TypeMeta `json:",inline"`
+	// deployment address
+	Deployment base.Bytes `protobuf:"bytes,1,opt,name=deployment,proto3,customtype=github.com/ovrclk/akash/types/base.Bytes" json:"deployment"`
+	// deployment group sequence
+	Group uint64 `protobuf:"varint,2,opt,name=group,proto3" json:"group,omitempty"`
+	// order sequence
+	Order uint64 `protobuf:"varint,3,opt,name=order,proto3" json:"order,omitempty"`
+	// provider address
+	Provider base.Bytes `protobuf:"bytes,4,opt,name=provider,proto3,customtype=github.com/ovrclk/akash/types/base.Bytes" json:"provider"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type ManifestSpec struct {
+	metav1.TypeMeta `json:",inline"`
+	LID             LeaseID `json:"LeaseID"`
+	// Placement profile name
+	Name string `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
+	// Service definitions
+	Services []*ManifestService `protobuf:"bytes,2,rep,name=services" json:"services,omitempty"`
+}
+
+type ManifestService struct {
+	// Service name
+	Name string `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
+	// Docker image
+	Image string   `protobuf:"bytes,2,opt,name=image,proto3" json:"image,omitempty"`
+	Args  []string `protobuf:"bytes,3,rep,name=args" json:"args,omitempty"`
+	Env   []string `protobuf:"bytes,4,rep,name=env" json:"env,omitempty"`
+	// Resource requirements
+	Unit ResourceUnit `protobuf:"bytes,5,opt,name=unit" json:"unit"`
+	// Number of instances
+	Count uint32 `protobuf:"varint,6,opt,name=count,proto3" json:"count,omitempty"`
+	// Overlay Network Links
+	Expose []*ManifestServiceExpose `protobuf:"bytes,7,rep,name=expose" json:"expose,omitempty"`
+}
+
+type ManifestServiceExpose struct {
+	Port         uint32 `protobuf:"varint,1,opt,name=port,proto3" json:"port,omitempty"`
+	ExternalPort uint32 `protobuf:"varint,2,opt,name=externalPort,proto3" json:"externalPort,omitempty"`
+	Proto        string `protobuf:"bytes,3,opt,name=proto,proto3" json:"proto,omitempty"`
+	Service      string `protobuf:"bytes,4,opt,name=service,proto3" json:"service,omitempty"`
+	Global       bool   `protobuf:"varint,5,opt,name=global,proto3" json:"global,omitempty"`
+	// accepted hostnames
+	Hosts []string `protobuf:"bytes,6,rep,name=hosts" json:"hosts,omitempty"`
+}
+
+// BEGIN EXCHANGE
+type ResourceUnit struct {
+	Cpu    uint32 `protobuf:"varint,1,opt,name=cpu,proto3" json:"cpu,omitempty"`
+	Memory uint32 `protobuf:"varint,2,opt,name=memory,proto3" json:"memory,omitempty"`
+	Disk   uint64 `protobuf:"varint,3,opt,name=disk,proto3" json:"disk,omitempty"`
+}
+
+type ManifestStatus struct {
+	State   string `json:"state,omitempty"`
+	Message string `json:"message,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type ManifestCRDList struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata"`
+	Items             []ManifestCRD `json:"items"`
+}
+
+func (m *ManifestCRD) LeaseID() types.LeaseID {
+	return types.LeaseID{
+		Deployment: m.Spec.LID.Deployment,
+		Group:      m.Spec.LID.Group,
+		Order:      m.Spec.LID.Order,
+		Provider:   m.Spec.LID.Provider,
+	}
+}
+
+func (m *ManifestCRD) ManifestGroup() *types.ManifestGroup {
+	json, err := m.Spec.Marshal()
+	if err != nil {
+		panic(err.Error())
+	}
+	group := &types.ManifestGroup{}
+	err = group.Unmarshal(json)
+	if err != nil {
+		panic(err.Error())
+	}
+	return group
+}
