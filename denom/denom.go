2577d974fdc0d005a36e508af95bb8d1b848a7cb
Aaron Stein <aaronstein12@gmail.com>
2018-07-16 10:48:29 -0700

    payments: factional amounts

--- denom/denom.go
+++ denom/denom.go
@@ -1,38 +1,19 @@
 package denom
 
 import (
-	"fmt"
 	"strconv"
-	"strings"
 )
 
 const (
-	miliAkash = 1
-	Akash     = 1000 * miliAkash
+	miliAkashPerAkash = 1000
 )
-
-var unitSuffixes = []struct {
-	symbol string
-	unit   uint64
-}{
-	{"A", Akash},
-	{"m", miliAkash},
-}
 
 // ToBase converts a unit of currency to its equivalent value in base denomination
 func ToBase(sval string) (uint64, error) {
-	for _, suffix := range unitSuffixes {
-		if !strings.HasSuffix(sval, suffix.symbol) {
-			continue
-		}
-		sval := strings.TrimSuffix(sval, suffix.symbol)
-		val, err := strconv.ParseUint(sval, 10, 64)
-		if err != nil {
-			return 0, err
-		}
-		amount := val * suffix.unit
-		return amount, nil
+	akash, err := strconv.ParseFloat(sval, 64)
+	if err != nil {
+		return 0, err
 	}
-
-	return 0, fmt.Errorf("unrecognized denomination %s", sval)
+	amount := akash * miliAkashPerAkash
+	return uint64(amount), nil
 }
