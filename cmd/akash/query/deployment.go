6cde2493abf41f9fc53277c9b6d436c1cbd08eb3
Aaron Stein <aaronstein12@gmail.com>
2018-07-24 16:14:21 -0700

    query: scope to tenant
    fixes: #304
    fixes: #285
    fixes: #286

--- cmd/akash/query/deployment.go
+++ cmd/akash/query/deployment.go
@@ -13,13 +13,20 @@ func queryDeploymentCommand() *cobra.Command {
 		Short: "query deployment",
 		RunE:  session.WithSession(session.RequireNode(doQueryDeploymentCommand)),
 	}
-
+	session.AddFlagKeyOptional(cmd, cmd.Flags())
 	return cmd
 }
 
 func doQueryDeploymentCommand(session session.Session, cmd *cobra.Command, args []string) error {
 	if len(args) == 0 {
-		return handleMessage(session.QueryClient().Deployments(session.Ctx()))
+		_, info, err := session.Signer()
+		if err != nil {
+			return err
+		}
+		if err := handleMessage(session.QueryClient().TenantDeployments(session.Ctx(), info.PubKey.Address().Bytes())); err != nil {
+			return err
+		}
+		return nil
 	}
 	for _, arg := range args {
 		key, err := keys.ParseDeploymentPath(arg)
