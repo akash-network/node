16ad5d524ab8fb6917b97abef3ff4d9dc8aea2e3
Adam Bozanich <adam.boz@gmail.com>
2018-07-30 14:43:04 -0700

    disable connection tests
    refs #338

--- cmd/akash/connection_test.go
+++ cmd/akash/connection_test.go
@@ -9,39 +9,46 @@ import (
 )
 
 func TestProviderCreate_NoNode(t *testing.T) {
+	testutil.Shrug(t, 338)
 	doTest_NoNode(t, providerCommand(),
 		createProviderCommand().Name(), "provider.yml", "-k", "keyname")
 }
 
 func TestProviderRun_NoNode(t *testing.T) {
+	testutil.Shrug(t, 338)
 	hexaddr := testutil.HexAddress(t)
 	doTest_NoNode(t, providerCommand(),
 		runCommand().Name(), hexaddr, "-k", "keyname")
 }
 
 func TestCreateDeployment_NoNode(t *testing.T) {
+	testutil.Shrug(t, 338)
 	path := "deployment.yaml"
 	doTest_NoNode(t, deploymentCommand(),
 		createDeploymentCommand().Name(), path, "-k", "keyname")
 }
 
 func TestCloseDeployment_NoNode(t *testing.T) {
+	testutil.Shrug(t, 338)
 	deployment := testutil.HexDeploymentAddress(t)
 	doTest_NoNode(t, deploymentCommand(),
 		closeDeploymentCommand().Name(), deployment, "-k", "keyname")
 }
 
 func TestMarketplace_NoNode(t *testing.T) {
+	testutil.Shrug(t, 338)
 	doTest_NoNode(t, marketplaceCommand())
 }
 
 func TestSend_NoNode(t *testing.T) {
+	testutil.Shrug(t, 338)
 	to := testutil.HexAddress(t)
 	doTest_NoNode(t, sendCommand(),
 		"1", to, "-k", "keyname")
 }
 
 func TestStatus_NoNode(t *testing.T) {
+	testutil.Shrug(t, 338)
 	doTest_NoNode(t, statusCommand())
 }
 
