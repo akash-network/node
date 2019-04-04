c6ce522fb1d80e934e57300e37b5f36e0291278f
Greg Osuri <me@gregosuri.com>
2019-04-04 10:37:22 -0700

    wip: fixing tests

--- cmd/common/monitor.go
+++ cmd/common/monitor.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/ovrclk/akash/marketplace"
 	tmlog "github.com/tendermint/tendermint/libs/log"
@@ -21,6 +22,7 @@ func MonitorMarketplace(ctx context.Context, log tmlog.Logger, client *tmclient.
 	defer func() { <-cdonech }()
 	go func() {
 		defer close(cdonech)
+		fmt.Println("cmn.Monitor: waiting on client to close")
 		client.Wait()
 	}()
 
@@ -29,15 +31,22 @@ func MonitorMarketplace(ctx context.Context, log tmlog.Logger, client *tmclient.
 		cancel()
 		return err
 	}
-	defer func() { <-monitor.Wait() }()
+	defer func() {
+		fmt.Println("cmn.Monitor: waiting on monitor to close")
+		<-monitor.Wait()
+		fmt.Println("cmn.Monitor: no wait monitor stopped")
+	}()
 
 	select {
 	case <-ctx.Done():
+		monitor.Stop()
 		client.UnsubscribeAll(context.Background(), "akash-cli")
 		client.Stop()
 	case <-cdonech:
+		fmt.Println("cmn.Monitor: cdonech closed")
 	}
 
+	fmt.Println("cmn.Monitor: the end")
 	cancel()
 	return nil
 }
--- marketplace/monitor.go
+++ marketplace/monitor.go
@@ -2,6 +2,7 @@ package marketplace
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/ovrclk/akash/txutil"
 	"github.com/ovrclk/akash/types"
@@ -41,11 +42,13 @@ func NewMonitor(ctx context.Context, log log.Logger, bus tmclient.EventsClient, 
 		donech:  make(chan struct{}),
 	}
 
+	fmt.Println("monitor --> before subscribe")
 	resC, err := m.bus.Subscribe(m.ctx, m.name, m.query.String())
 	if err != nil {
 		<-m.donech
 		return nil, err
 	}
+	fmt.Println("monitor --> after subscribe")
 
 	go m.runListener(resC, m.handler)
 
@@ -53,50 +56,63 @@ func NewMonitor(ctx context.Context, log log.Logger, bus tmclient.EventsClient, 
 }
 
 func (m *monitor) Stop() error {
+	fmt.Println("monitor --> stop")
+	close(m.donech)
 	return m.bus.Unsubscribe(m.ctx, m.name, m.query.String())
 }
 
 func (m *monitor) Wait() <-chan struct{} {
+	fmt.Println("monitor --> wait called")
 	return m.donech
 }
 
 func (m *monitor) runListener(ch <-chan ctypes.ResultEvent, h Handler) {
-	defer close(m.donech)
+	// defer func() {
+	// 	fmt.Println("monitor --> closing(m.donech)")
+	// 	close(m.donech)
+	// }()
 
 	for {
-		ed := <-ch
-		evt, ok := ed.Data.(tmtmtypes.EventDataTx)
-		if !ok {
-			continue
-		}
+		fmt.Println("monitor --> wait on event")
+		select {
+		case ed := <-ch:
+			evt, ok := ed.Data.(tmtmtypes.EventDataTx)
+			if !ok {
+				fmt.Println("monitor --> runListener not ok EventDataTx", evt)
+				continue
+			}
 
-		tx, err := txutil.ProcessTx(evt.Tx)
-		if err != nil {
-			m.log.Error("ProcessTx", "error", err)
-			continue
-		}
+			tx, err := txutil.ProcessTx(evt.Tx)
+			if err != nil {
+				m.log.Error("ProcessTx", "error", err)
+				continue
+			}
 
-		switch tx := tx.Payload.GetPayload().(type) {
-		case *types.TxPayload_TxSend:
-			h.OnTxSend(tx.TxSend)
-		case *types.TxPayload_TxCreateProvider:
-			h.OnTxCreateProvider(tx.TxCreateProvider)
-		case *types.TxPayload_TxCreateDeployment:
-			h.OnTxCreateDeployment(tx.TxCreateDeployment)
-		case *types.TxPayload_TxUpdateDeployment:
-			h.OnTxUpdateDeployment(tx.TxUpdateDeployment)
-		case *types.TxPayload_TxCreateOrder:
-			h.OnTxCreateOrder(tx.TxCreateOrder)
-		case *types.TxPayload_TxCreateFulfillment:
-			h.OnTxCreateFulfillment(tx.TxCreateFulfillment)
-		case *types.TxPayload_TxCreateLease:
-			h.OnTxCreateLease(tx.TxCreateLease)
-		case *types.TxPayload_TxCloseDeployment:
-			h.OnTxCloseDeployment(tx.TxCloseDeployment)
-		case *types.TxPayload_TxCloseFulfillment:
-			h.OnTxCloseFulfillment(tx.TxCloseFulfillment)
-		case *types.TxPayload_TxCloseLease:
-			h.OnTxCloseLease(tx.TxCloseLease)
+			switch tx := tx.Payload.GetPayload().(type) {
+			case *types.TxPayload_TxSend:
+				h.OnTxSend(tx.TxSend)
+			case *types.TxPayload_TxCreateProvider:
+				h.OnTxCreateProvider(tx.TxCreateProvider)
+			case *types.TxPayload_TxCreateDeployment:
+				h.OnTxCreateDeployment(tx.TxCreateDeployment)
+			case *types.TxPayload_TxUpdateDeployment:
+				h.OnTxUpdateDeployment(tx.TxUpdateDeployment)
+			case *types.TxPayload_TxCreateOrder:
+				h.OnTxCreateOrder(tx.TxCreateOrder)
+			case *types.TxPayload_TxCreateFulfillment:
+				h.OnTxCreateFulfillment(tx.TxCreateFulfillment)
+			case *types.TxPayload_TxCreateLease:
+				h.OnTxCreateLease(tx.TxCreateLease)
+			case *types.TxPayload_TxCloseDeployment:
+				h.OnTxCloseDeployment(tx.TxCloseDeployment)
+			case *types.TxPayload_TxCloseFulfillment:
+				h.OnTxCloseFulfillment(tx.TxCloseFulfillment)
+			case *types.TxPayload_TxCloseLease:
+				h.OnTxCloseLease(tx.TxCloseLease)
+			}
+			fmt.Println("monitor --> done processing event")
+		case <-m.donech:
+			return
 		}
 	}
 }
