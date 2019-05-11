8187d9e769119131c38042def6dd1adfe4664559
Greg Osuri <me@gregosuri.com>
2019-05-11 00:31:46 -0700

    ...

--- cmd/akash/status.go
+++ cmd/akash/status.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"strconv"
+	"time"
 
+	"github.com/dustin/go-humanize"
+	"github.com/gosuri/uitable"
 	"github.com/ovrclk/akash/cmd/akash/session"
+	. "github.com/ovrclk/akash/util"
 	"github.com/spf13/cobra"
 )
 
@@ -19,19 +22,40 @@ func statusCommand() *cobra.Command {
 	return cmd
 }
 
-func doStatusCommand(session session.Session, cmd *cobra.Command, args []string) error {
-	client := session.Client()
-
+func doStatusCommand(s session.Session, cmd *cobra.Command, args []string) error {
+	client := s.Client()
 	result, err := client.Status()
 	if err != nil {
 		return err
 	}
 
-	fmt.Printf("Block: %v\nBlock Hash: %v\n", result.SyncInfo.LatestBlockHeight, result.SyncInfo.LatestBlockHash)
+	version := result.NodeInfo.Version
+	syncHeight := result.SyncInfo.LatestBlockHeight
+	syncHash := X(result.SyncInfo.LatestBlockHash)
+	syncBlockTime := result.SyncInfo.LatestBlockTime
 
-	if result.SyncInfo.LatestBlockHeight == 0 {
-		os.Exit(1)
+	printerDat := session.NewPrinterDataKV().
+		AddResultKV("node_version", version).
+		AddResultKV("latest_block_height", strconv.FormatInt(syncHeight, 10))
+	if syncHeight > 0 {
+		printerDat.
+			AddResultKV("latest_block_hash", syncHash).
+			AddResultKV("lastest_block_time", syncBlockTime.Format(time.RFC3339))
 	}
+	printerDat.Raw = result
 
-	return nil
+	return s.Mode().
+		When(session.ModeTypeInteractive, func() error {
+			t := uitable.New().
+				AddRow("Node Version: ", version).
+				AddRow("Latest Block (Height): ", humanize.Comma(syncHeight))
+			if syncHeight > 0 {
+				t.AddRow("Latest Block Hash: ", syncHash).
+					AddRow("Last Block Created: ", humanize.Time(syncBlockTime))
+			}
+			return session.NewIPrinter(nil).AddText("").Add(t).Flush()
+		}).
+		When(session.ModeTypeText, func() error { return session.NewTextPrinter(printerDat, nil).Flush() }).
+		When(session.ModeTypeJSON, func() error { return session.NewJSONPrinter(printerDat, nil).Flush() }).
+		Run()
 }
