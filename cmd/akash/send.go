f13622649139c6496e57b6105671a3cb6333d6ef
Greg Osuri <me@gregosuri.com>
2019-05-11 00:04:49 -0700

    ...

--- cmd/akash/send.go
+++ cmd/akash/send.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"strconv"
 
+	"github.com/gosuri/uitable"
 	"github.com/ovrclk/akash/cmd/akash/session"
 	"github.com/ovrclk/akash/denom"
+	"github.com/ovrclk/akash/errors"
 	"github.com/ovrclk/akash/keys"
 	"github.com/ovrclk/akash/types"
+	. "github.com/ovrclk/akash/util"
 	"github.com/spf13/cobra"
 )
 
 func sendCommand() *cobra.Command {
 
 	cmd := &cobra.Command{
-		Use:   "send [amount] [to account]",
+		Use:   "send [amount] [receiver]",
 		Short: "Send tokens",
 		RunE: session.WithSession(
 			session.RequireKey(session.RequireNode(doSendCommand))),
@@ -25,24 +28,41 @@ func sendCommand() *cobra.Command {
 	return cmd
 }
 
-func doSendCommand(session session.Session, cmd *cobra.Command, args []string) error {
-	txclient, err := session.TxClient()
+func doSendCommand(s session.Session, cmd *cobra.Command, args []string) error {
+	txclient, err := s.TxClient()
 	if err != nil {
 		return err
 	}
 
-	amount, err := denom.ToBase(args[0])
+	var argAmount, argTo string
+	if len(args) == 2 {
+		argAmount = args[0]
+		argTo = args[1]
+	}
+
+	argAmount = s.Mode().Ask().StringVar(argAmount, "Amount (required): ", true)
+	if len(argAmount) == 0 {
+		return errors.NewArgumentError("amount")
+	}
+	argTo = s.Mode().Ask().StringVar(argTo, "Receiver Address (required): ", true)
+	if len(argTo) == 0 {
+		return errors.NewArgumentError("receiver")
+	}
+
+	amount, err := denom.ToBase(argAmount)
 	if err != nil {
 		return err
 	}
 
-	to, err := keys.ParseAccountPath(args[1])
+	to, err := keys.ParseAccountPath(argTo)
 	if err != nil {
 		return err
 	}
+
+	fromAddr := txclient.Key().GetPubKey().Address().Bytes()
 
 	result, err := txclient.BroadcastTxCommit(&types.TxSend{
-		From:   txclient.Key().GetPubKey().Address().Bytes(),
+		From:   fromAddr,
 		To:     to.ID(),
 		Amount: amount,
 	})
@@ -50,7 +70,32 @@ func doSendCommand(session session.Session, cmd *cobra.Command, args []string) e
 		return err
 	}
 
-	fmt.Printf("Sent %v tokens to %s in block %v\n", args[0], to.ID(), result.Height)
+	printerDat := session.NewPrinterDataKV().
+		AddResultKV("from", X(fromAddr)).
+		AddResultKV("to", argTo).
+		AddResultKV("amount", argAmount).
+		AddResultKV("block", strconv.FormatInt(result.Height, 10)).
+		AddResultKV("hash", X(result.Hash))
 
-	return nil
+	printerDat.Raw = result
+
+	return s.Mode().
+		When(session.ModeTypeInteractive, func() error {
+			res := printerDat.Result[0]
+			t := uitable.New().
+				AddRow("From: ", res["from"]).
+				AddRow("To: ", res["to"]).
+				AddRow("Amount: ", res["amount"]).
+				AddRow("Block (Height): ", res["block"]).
+				AddRow("Hash: ", res["hash"])
+
+			return session.NewIPrinter(nil).
+				AddText("").
+				AddTitle("Tokens transferred successfully").
+				Add(t).
+				Flush()
+		}).
+		When(session.ModeTypeText, func() error { return session.NewTextPrinter(printerDat, nil).Flush() }).
+		When(session.ModeTypeJSON, func() error { return session.NewJSONPrinter(printerDat, nil).Flush() }).
+		Run()
 }
