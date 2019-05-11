a6cc55a11967df8e428d8cf4ea273221989bf356
Greg Osuri <me@gregosuri.com>
2019-05-10 21:46:12 -0700

    ...

--- cmd/akash/query/fulfillment.go
+++ cmd/akash/query/fulfillment.go
@@ -1,8 +1,12 @@
 package query
 
 import (
+	humanize "github.com/dustin/go-humanize"
+	"github.com/gosuri/uitable"
 	"github.com/ovrclk/akash/cmd/akash/session"
 	"github.com/ovrclk/akash/keys"
+	"github.com/ovrclk/akash/types"
+	"github.com/ovrclk/akash/util/uiutil"
 	"github.com/spf13/cobra"
 )
 
@@ -17,18 +21,60 @@ func queryFulfillmentCommand() *cobra.Command {
 	return cmd
 }
 
-func doQueryFulfillmentCommand(session session.Session, cmd *cobra.Command, args []string) error {
+func doQueryFulfillmentCommand(s session.Session, cmd *cobra.Command, args []string) error {
+	fulfillments := make([]*types.Fulfillment, 0)
+	printerDat := session.NewPrinterDataList()
+	rawDat := make([]interface{}, 0, 0)
+
 	if len(args) == 0 {
-		return handleMessage(session.QueryClient().Fulfillments(session.Ctx()))
-	}
-	for _, arg := range args {
-		key, err := keys.ParseFulfillmentPath(arg)
+		res, err := s.QueryClient().Fulfillments(s.Ctx())
 		if err != nil {
 			return err
 		}
-		if err := handleMessage(session.QueryClient().Fulfillment(session.Ctx(), key.ID())); err != nil {
-			return err
+		fulfillments = res.Items
+	} else {
+		for _, arg := range args {
+			key, err := keys.ParseFulfillmentPath(arg)
+			if err != nil {
+				return err
+			}
+			fulfillment, err := s.QueryClient().Fulfillment(s.Ctx(), key.ID())
+			if err != nil {
+				return err
+			}
+			fulfillments = append(fulfillments, fulfillment)
 		}
 	}
-	return nil
+
+	for _, f := range fulfillments {
+		printerDat.AddResultList(makePrinterResultFulfillment(f))
+		rawDat = append(rawDat, f)
+	}
+	printerDat.Raw = rawDat
+
+	return s.Mode().
+		When(session.ModeTypeInteractive, func() error {
+			t := uitable.New().AddRow(
+				uiutil.NewTitle("Fulfillment ID (Deployment/Group/Order/Provider)").String(),
+				uiutil.NewTitle("Price").String(),
+				uiutil.NewTitle("State").String(),
+			)
+			t.Wrap = true
+			for _, f := range fulfillments {
+				res := makePrinterResultFulfillment(f)
+				t.AddRow(res["fulfillment"], res["price"], res["state"])
+			}
+			return session.NewIPrinter(nil).AddText("").Add(t).Flush()
+		}).
+		When(session.ModeTypeText, func() error { return session.NewTextPrinter(printerDat, nil).Flush() }).
+		When(session.ModeTypeJSON, func() error { return session.NewJSONPrinter(printerDat, nil).Flush() }).
+		Run()
+}
+
+func makePrinterResultFulfillment(f *types.Fulfillment) session.PrinterResult {
+	return session.PrinterResult{
+		"fulfillment": f.FulfillmentID.String(),
+		"price":       humanize.Comma(int64(f.Price)),
+		"state":       f.State.String(),
+	}
 }
--- cmd/akash/query/lease.go
+++ cmd/akash/query/lease.go
@@ -1,8 +1,14 @@
 package query
 
 import (
+	"fmt"
+
+	humanize "github.com/dustin/go-humanize"
+	"github.com/gosuri/uitable"
 	"github.com/ovrclk/akash/cmd/akash/session"
 	"github.com/ovrclk/akash/keys"
+	"github.com/ovrclk/akash/types"
+	"github.com/ovrclk/akash/util/uiutil"
 	"github.com/spf13/cobra"
 )
 
@@ -17,25 +23,77 @@ func queryLeaseCommand() *cobra.Command {
 	return cmd
 }
 
-func doQueryLeaseCommand(session session.Session, cmd *cobra.Command, args []string) error {
-	if len(args) == 0 {
-		_, info, err := session.Signer()
+func doQueryLeaseCommand(s session.Session, cmd *cobra.Command, args []string) error {
+	leases := make([]*types.Lease, 0)
+	printerDat := session.NewPrinterDataList()
+	rawDat := make([]interface{}, 0, 0)
+
+	var hasSigner, hasIDs bool
+	hasIDs = len(args) > 0
+	_, info, err := s.Signer()
+	if err == nil {
+		hasSigner = true
+	}
+
+	switch {
+	case hasSigner == false && hasIDs == false:
+		var id string
+		id = s.Mode().Ask().StringVar(id, "Lease ID (required): ", true)
+		if len(id) == 0 {
+			return fmt.Errorf("required argument missing: id")
+		}
+		args = []string{id}
+		hasIDs = true
+		fallthrough
+	case hasIDs:
+		for _, arg := range args {
+			key, err := keys.ParseLeasePath(arg)
+			if err != nil {
+				return err
+			}
+			lease, err := s.QueryClient().Lease(s.Ctx(), key.ID())
+			if err != nil {
+				return err
+			}
+			leases = append(leases, lease)
+		}
+	case hasSigner:
+		res, err := s.QueryClient().TenantLeases(s.Ctx(), info.GetPubKey().Address().Bytes())
 		if err != nil {
 			return err
 		}
-		if err := handleMessage(session.QueryClient().TenantLeases(session.Ctx(), info.GetPubKey().Address().Bytes())); err != nil {
-			return err
-		}
-		return nil
+		leases = res.Items
 	}
-	for _, arg := range args {
-		key, err := keys.ParseLeasePath(arg)
-		if err != nil {
-			return err
-		}
-		if err := handleMessage(session.QueryClient().Lease(session.Ctx(), key.ID())); err != nil {
-			return err
-		}
+
+	for _, l := range leases {
+		printerDat.AddResultList(makePrinterResultLease(l))
+		rawDat = append(rawDat, l)
 	}
-	return nil
+	printerDat.Raw = rawDat
+
+	return s.Mode().
+		When(session.ModeTypeInteractive, func() error {
+			t := uitable.New().AddRow(
+				uiutil.NewTitle("Lease ID (Deployment/Group/Order/Provider)").String(),
+				uiutil.NewTitle("Price").String(),
+				uiutil.NewTitle("State").String(),
+			)
+			t.Wrap = true
+			for _, l := range leases {
+				res := makePrinterResultLease(l)
+				t.AddRow(res["lease"], res["price"], res["state"])
+			}
+			return session.NewIPrinter(nil).AddText("").Add(t).Flush()
+		}).
+		When(session.ModeTypeText, func() error { return session.NewTextPrinter(printerDat, nil).Flush() }).
+		When(session.ModeTypeJSON, func() error { return session.NewJSONPrinter(printerDat, nil).Flush() }).
+		Run()
+}
+
+func makePrinterResultLease(lease *types.Lease) session.PrinterResult {
+	return session.PrinterResult{
+		"lease": lease.LeaseID.String(),
+		"price": humanize.Comma(int64(lease.Price)),
+		"state": lease.State.String(),
+	}
 }
