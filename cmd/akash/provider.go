8b8b243d2972b8d7294ad852773369e5729cbc07
Anil Kumar Kammari <anil@vitwit.com>
2019-12-13 17:36:39 +0530

    Fix querying only passive providers (#433)

--- cmd/akash/provider.go
+++ cmd/akash/provider.go
@@ -252,6 +252,7 @@ func providerStatusCommand() *cobra.Command {
 func doProviderStatusCommand(session session.Session, cmd *cobra.Command, args []string) error {
 	providerState, _ := cmd.Flags().GetString("state")
 	plist, err := session.QueryClient().Providers(session.Ctx())
+
 	if err != nil {
 		return err
 	}
@@ -307,8 +308,11 @@ func doProviderStatusCommand(session session.Session, cmd *cobra.Command, args [
 	printer := session.Mode().Printer()
 	var active, passive []*outputItem
 	for _, o := range output {
-		if len(o.Error) == 0 && providerState != "passive" {
-			active = append(active, o)
+		if len(o.Error) == 0 {
+			if providerState != "passive" {
+				active = append(active, o)
+			}
+
 			continue
 		}
 
