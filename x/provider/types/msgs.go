d95e8c5c1a277198a7fbbaf12b1fca3c02b9233d
anilCSE <anil@vitwit.com>
2020-02-26 18:34:06 +0530

    Fix provider msgs lint issues

--- x/provider/types/msgs.go
+++ x/provider/types/msgs.go
@@ -4,10 +4,16 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
+// MsgCreate defines an SDK message for creating a provider
 type MsgCreate Provider
 
+// Route implements the sdk.Msg interface
 func (msg MsgCreate) Route() string { return RouterKey }
+
+// Type implements the sdk.Msg interface
 func (msg MsgCreate) Type() string  { return "create" }
+
+// ValidateBasic does basic validation of a HostURI
 func (msg MsgCreate) ValidateBasic() error {
 	switch {
 	case len(msg.HostURI) == 0:
@@ -27,10 +33,16 @@ func (msg MsgCreate) GetSigners() []sdk.AccAddress {
 	return []sdk.AccAddress{msg.Owner}
 }
 
+// MsgUpdate defines an SDK message for updating a provider
 type MsgUpdate Provider
 
+// Route implements the sdk.Msg interface
 func (msg MsgUpdate) Route() string { return RouterKey }
+
+// Type implements the sdk.Msg interface
 func (msg MsgUpdate) Type() string  { return "update" }
+
+// ValidateBasic does basic validation of a ProviderURI
 func (msg MsgUpdate) ValidateBasic() error {
 	switch {
 	case len(msg.HostURI) == 0:
@@ -50,12 +62,18 @@ func (msg MsgUpdate) GetSigners() []sdk.AccAddress {
 	return []sdk.AccAddress{msg.Owner}
 }
 
+// MsgDelete defines an SDK message for deleting a provider
 type MsgDelete struct {
 	Owner sdk.AccAddress `json:"owner"`
 }
 
+// Route implements the sdk.Msg interface
 func (msg MsgDelete) Route() string { return RouterKey }
+
+// Type implements the sdk.Msg interface
 func (msg MsgDelete) Type() string  { return "delete" }
+
+// ValidateBasic does basic validation
 func (msg MsgDelete) ValidateBasic() error {
 	return nil
 }
