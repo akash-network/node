ea44ce6e9860c55400a6fba9e4e0cbc0763f8152
Akhil Kumar P <36399231+akhilkumarpilli@users.noreply.github.com>
2020-03-03 22:25:07 +0530

    Add proper validation for provider url (#449)
    * Fix hostURI validation in provider module
    * Remove todo comment in provider validation
    * Add absoulte url validation in provider
    * Update error message in provider msg
    
    Co-authored-by: Anil Kumar Kammari <anil@vitwit.com>

--- x/provider/types/errors.go
+++ x/provider/types/errors.go
@@ -6,5 +6,7 @@ import (
 
 var (
 	// ErrInvalidProviderURI register error code for invalid provider uri
-	ErrInvalidProviderURI = sdkerrors.Register(ModuleName, 1, "invalid provider: empty host uri")
+	ErrInvalidProviderURI = sdkerrors.Register(ModuleName, 1, "invalid provider: invalid host uri")
+	// ErrNotAbsProviderURI register error code for not absolute provider uri
+	ErrNotAbsProviderURI = sdkerrors.Register(ModuleName, 2, "invalid provider: not absoulte host uri")
 )
