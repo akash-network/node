4072a3fa819238f6760b1b249079d873cb17a748
charymalloju <chary@vitwit.com>
2020-03-21 14:47:40 +0530

    fix provider simulation params

--- x/provider/simulation/params.go
+++ x/provider/simulation/params.go
@@ -1,33 +1,23 @@
 package simulation
 
-// DONTCOVER
+// const (
+// 	keyProvider  = "Provider"
+// 	keyProviders = "Providers"
+// )
 
-import (
-	"fmt"
-	"math/rand"
-
-	"github.com/cosmos/cosmos-sdk/x/simulation"
-	"github.com/ovrclk/akash/x/provider/types"
-)
-
-const (
-	keyProvider  = "Provider"
-	keyProviders = "Providers"
-)
-
-// ParamChanges defines the parameters that can be modified by param change proposals
-// on the simulation
-func ParamChanges(r *rand.Rand) []simulation.ParamChange {
-	return []simulation.ParamChange{
-		simulation.NewSimParamChange(types.ModuleName, keyProvider,
-			func(r *rand.Rand) string {
-				return fmt.Sprintf("%d", GenValidator(r))
-			},
-		),
-		simulation.NewSimParamChange(types.ModuleName, keyProviders,
-			func(r *rand.Rand) string {
-				return fmt.Sprintf("\"%d\"", GenValidators(r))
-			},
-		),
-	}
-}
+// // ParamChanges defines the parameters that can be modified by param change proposals
+// // on the simulation
+// func ParamChanges(r *rand.Rand) []simulation.ParamChange {
+// 	return []simulation.ParamChange{
+// 		simulation.NewSimParamChange(types.ModuleName, keyProvider,
+// 			func(r *rand.Rand) string {
+// 				return fmt.Sprintf("%d", GenValidator(r))
+// 			},
+// 		),
+// 		simulation.NewSimParamChange(types.ModuleName, keyProviders,
+// 			func(r *rand.Rand) string {
+// 				return fmt.Sprintf("\"%d\"", GenValidators(r))
+// 			},
+// 		),
+// 	}
+// }
