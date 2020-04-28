8d78eeff544f163fa78338a28842106aebdf4598
Greg Osuri <me@gregosuri.com>
2020-04-28 08:05:15 -0700

    add run scripts with helm for single node on kube (#504)
    Signed-off-by: Greg Osuri <me@gregosuri.com>

--- _run/common.mk
+++ _run/common.mk
@@ -3,19 +3,26 @@ AKASH_ROOT := ../..
 AKASH  := $(AKASH)/akash
 AKASHD := $(AKASHD)/akashd
 
-DATA_ROOT = data
-NODE_ROOT = $(DATA_ROOT)/node
+DATA_ROOT 	= cache
+NODE_HOME 	= $(DATA_ROOT)/node
+CLIENT_HOME = $(DATA_ROOT)/client
 
 all:
 	(cd $(AKASH_ROOT) && make all)
+
 build:
 	(cd $(AKASH_ROOT) && make build)
+
 akash:
 	(cd $(AKASH_ROOT) && make akash)
+
 akashd:
 	(cd $(AKASH_ROOT) && make akashd)
+
 image-minikube:
 	(cd $(AKASH_ROOT) && make image-minikube)
 
 clean:
 	rm -rf $(DATA_ROOT)
+
+.PHONY: all build akash akashd image-minikube
