b60f71ee0d1d5a5ef046173b96a8e3f89cb1d62b
Akhil Kumar P <36399231+akhilkumarpilli@users.noreply.github.com>
2020-04-30 22:03:47 +0530

    Implement github actions for simulation tests (#512)
    * Add import export test in simapp
    * Modify appimportexport test
    * Add simulation actions and update commands in makefile

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 BINS       := akashctl akashd
 IMAGE_BINS := _build/akashctl _build/akashd
+APP_DIR := ./app
 
 GO := GO111MODULE=on go
 
@@ -142,16 +143,6 @@ clean:
 	kubetypes \
 	install
 
-test-simapp:
-	go test ./app \
-	-run=TestFullAppSimulation \
-	-Enabled=true \
-	-NumBlocks=50 \
-	-BlockSize=100 \
-	-Commit=true \
-	-Seed=99 \
-	-v -timeout 24h
-
 update-swagger-docs:
 	statik -src=cmd/swagger-ui -dest=cmd -f -m
 	@if [ -n "$(git status --porcelain)" ]; then \
@@ -160,3 +151,27 @@ update-swagger-docs:
     else \
     	echo "\033[92mSwagger docs are in sync\033[0m";\
     fi
+
+###############################################################################
+###                           Simulation                                    ###
+###############################################################################
+
+test-sim-fullapp:
+	@echo "Running app simulation test..."
+	@go test -mod=readonly ${APP_DIR} -run=TestFullAppSimulation -Enabled=true \
+		-NumBlocks=50 -BlockSize=100 -Commit=true -Seed=99 -Period=5 -v -timeout 24h
+
+test-sim-nondeterminism:
+	@echo "Running non-determinism test. This may take several minutes..."
+	@go test -mod=readonly $(APP_DIR) -run TestAppStateDeterminism -Enabled=true \
+		-NumBlocks=50 -BlockSize=100 -Commit=true -Period=0 -v -timeout 24h
+
+test-sim-import-export:
+	@echo "Running application import/export simulation..."
+	@go test -mod=readonly $(APP_DIR) -run=TestAppImportExport -Enabled=true \
+		-NumBlocks=50 -BlockSize=100 -Commit=true -Seed=99 -Period=5 -v -timeout 24h
+
+test-sim-after-import:
+	@echo "Running application simulation-after-import..."
+	@go test -mod=readonly $(APP_DIR) -run=TestAppSimulationAfterImport -Enabled=true \
+		-NumBlocks=50 -BlockSize=100 -Commit=true -Seed=99 -Period=5 -v -timeout 24h
---
+++ app/export.go
@@ -0,0 +1,167 @@
+package app
+
+import (
+	"encoding/json"
+	"log"
+
+	abci "github.com/tendermint/tendermint/abci/types"
+	tmtypes "github.com/tendermint/tendermint/types"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/slashing"
+	"github.com/cosmos/cosmos-sdk/x/staking"
+	"github.com/cosmos/cosmos-sdk/x/staking/exported"
+)
+
+// ExportAppStateAndValidators exports the state of the application for a genesis
+// file.
+func (app *AkashApp) ExportAppStateAndValidators(
+	forZeroHeight bool, jailWhiteList []string,
+) (appState json.RawMessage, validators []tmtypes.GenesisValidator, err error) {
+
+	// as if they could withdraw from the start of the next block
+	ctx := app.NewContext(true, abci.Header{Height: app.LastBlockHeight()})
+
+	if forZeroHeight {
+		app.prepForZeroHeightGenesis(ctx, jailWhiteList)
+	}
+
+	genState := app.mm.ExportGenesis(ctx)
+	appState, err = codec.MarshalJSONIndent(app.cdc, genState)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	validators = staking.WriteValidators(ctx, app.keeper.staking)
+	return appState, validators, nil
+}
+
+// prepare for fresh start at zero height
+// NOTE zero height genesis is a temporary feature which will be deprecated
+//      in favour of export at a block height
+func (app *AkashApp) prepForZeroHeightGenesis(ctx sdk.Context, jailWhiteList []string) {
+	applyWhiteList := false
+
+	//Check if there is a whitelist
+	if len(jailWhiteList) > 0 {
+		applyWhiteList = true
+	}
+
+	whiteListMap := make(map[string]bool)
+
+	for _, addr := range jailWhiteList {
+		_, err := sdk.ValAddressFromBech32(addr)
+		if err != nil {
+			log.Fatal(err)
+		}
+		whiteListMap[addr] = true
+	}
+
+	/* Just to be safe, assert the invariants on current state. */
+	app.keeper.crisis.AssertInvariants(ctx)
+
+	/* Handle fee distribution state. */
+
+	// withdraw all validator commission
+	app.keeper.staking.IterateValidators(ctx, func(_ int64, val exported.ValidatorI) (stop bool) {
+		_, _ = app.keeper.distr.WithdrawValidatorCommission(ctx, val.GetOperator())
+		return false
+	})
+
+	// withdraw all delegator rewards
+	dels := app.keeper.staking.GetAllDelegations(ctx)
+	for _, delegation := range dels {
+		_, _ = app.keeper.distr.WithdrawDelegationRewards(ctx, delegation.DelegatorAddress, delegation.ValidatorAddress)
+	}
+
+	// clear validator slash events
+	app.keeper.distr.DeleteAllValidatorSlashEvents(ctx)
+
+	// clear validator historical rewards
+	app.keeper.distr.DeleteAllValidatorHistoricalRewards(ctx)
+
+	// set context height to zero
+	height := ctx.BlockHeight()
+	ctx = ctx.WithBlockHeight(0)
+
+	// reinitialize all validators
+	app.keeper.staking.IterateValidators(ctx, func(_ int64, val exported.ValidatorI) (stop bool) {
+
+		// donate any unwithdrawn outstanding reward fraction tokens to the community pool
+		scraps := app.keeper.distr.GetValidatorOutstandingRewards(ctx, val.GetOperator())
+		feePool := app.keeper.distr.GetFeePool(ctx)
+		feePool.CommunityPool = feePool.CommunityPool.Add(scraps...)
+		app.keeper.distr.SetFeePool(ctx, feePool)
+
+		app.keeper.distr.Hooks().AfterValidatorCreated(ctx, val.GetOperator())
+		return false
+	})
+
+	// reinitialize all delegations
+	for _, del := range dels {
+		app.keeper.distr.Hooks().BeforeDelegationCreated(ctx, del.DelegatorAddress, del.ValidatorAddress)
+		app.keeper.distr.Hooks().AfterDelegationModified(ctx, del.DelegatorAddress, del.ValidatorAddress)
+	}
+
+	// reset context height
+	ctx = ctx.WithBlockHeight(height)
+
+	/* Handle staking state. */
+
+	// iterate through redelegations, reset creation height
+	app.keeper.staking.IterateRedelegations(ctx, func(_ int64, red staking.Redelegation) (stop bool) {
+		for i := range red.Entries {
+			red.Entries[i].CreationHeight = 0
+		}
+		app.keeper.staking.SetRedelegation(ctx, red)
+		return false
+	})
+
+	// iterate through unbonding delegations, reset creation height
+	app.keeper.staking.IterateUnbondingDelegations(ctx, func(_ int64, ubd staking.UnbondingDelegation) (stop bool) {
+		for i := range ubd.Entries {
+			ubd.Entries[i].CreationHeight = 0
+		}
+		app.keeper.staking.SetUnbondingDelegation(ctx, ubd)
+		return false
+	})
+
+	// Iterate through validators by power descending, reset bond heights, and
+	// update bond intra-tx counters.
+	store := ctx.KVStore(app.keys[staking.StoreKey])
+	iter := sdk.KVStoreReversePrefixIterator(store, staking.ValidatorsKey)
+	counter := int16(0)
+
+	for ; iter.Valid(); iter.Next() {
+		addr := sdk.ValAddress(iter.Key()[1:])
+		validator, found := app.keeper.staking.GetValidator(ctx, addr)
+		if !found {
+			panic("expected validator, not found")
+		}
+
+		validator.UnbondingHeight = 0
+		if applyWhiteList && !whiteListMap[addr.String()] {
+			validator.Jailed = true
+		}
+
+		app.keeper.staking.SetValidator(ctx, validator)
+		counter++
+	}
+
+	iter.Close()
+
+	_ = app.keeper.staking.ApplyAndReturnValidatorSetUpdates(ctx)
+
+	/* Handle slashing state. */
+
+	// reset start height on signing infos
+	app.keeper.slashing.IterateValidatorSigningInfos(
+		ctx,
+		func(addr sdk.ConsAddress, info slashing.ValidatorSigningInfo) (stop bool) {
+			info.StartHeight = 0
+			app.keeper.slashing.SetValidatorSigningInfo(ctx, addr, info)
+			return false
+		},
+	)
+}
