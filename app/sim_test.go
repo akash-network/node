99bbc1c681c760049e309979e5fe155f7e1b3df2
akhilkumarpilli <akhilkumar7947@gmail.com>
2020-04-30 18:12:59 +0530

    Add simulation actions and update commands in makefile

--- app/sim_test.go
+++ app/sim_test.go
@@ -248,8 +248,8 @@ func TestAppStateDeterminism(t *testing.T) {
 	config.AllInvariants = false
 	config.ChainID = helpers.SimAppChainID
 
-	numSeeds := 3
-	numTimesToRunPerSeed := 5
+	numSeeds := 2
+	numTimesToRunPerSeed := 2
 	appHashList := make([]json.RawMessage, numTimesToRunPerSeed)
 
 	for i := 0; i < numSeeds; i++ {
