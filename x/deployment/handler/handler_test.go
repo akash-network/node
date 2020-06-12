eb924a2a7b1122f8580fbfdf0af33399b1d61ba5
Artur Troian <troian@users.noreply.github.com>
2020-06-12 11:53:19 -0400

    feat: unit tests for handlers (#654)
    fixes #648

---
+++ x/deployment/handler/handler_test.go
@@ -0,0 +1,234 @@
+package handler_test
+
+import (
+	"testing"
+
+	"github.com/cosmos/cosmos-sdk/store"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
+	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/tendermint/tendermint/libs/rand"
+	dbm "github.com/tendermint/tm-db"
+
+	"github.com/tendermint/tendermint/libs/log"
+
+	"github.com/ovrclk/akash/app"
+	"github.com/ovrclk/akash/testutil"
+	atypes "github.com/ovrclk/akash/types"
+	"github.com/ovrclk/akash/x/deployment/handler"
+	"github.com/ovrclk/akash/x/deployment/keeper"
+	"github.com/ovrclk/akash/x/deployment/types"
+	mkeeper "github.com/ovrclk/akash/x/market/keeper"
+	mtypes "github.com/ovrclk/akash/x/market/types"
+)
+
+type testSuite struct {
+	t       *testing.T
+	ms      sdk.CommitMultiStore
+	ctx     sdk.Context
+	mkeeper mkeeper.Keeper
+	dkeeper keeper.Keeper
+	handler sdk.Handler
+}
+
+func setupTestSuite(t *testing.T) *testSuite {
+	suite := &testSuite{
+		t: t,
+	}
+
+	testutil.AccAddress(t)
+	dKey := sdk.NewKVStoreKey(types.StoreKey)
+	mKey := sdk.NewKVStoreKey(mtypes.StoreKey)
+
+	db := dbm.NewMemDB()
+	suite.ms = store.NewCommitMultiStore(db)
+	suite.ms.MountStoreWithDB(dKey, sdk.StoreTypeIAVL, db)
+	suite.ms.MountStoreWithDB(mKey, sdk.StoreTypeIAVL, db)
+
+	err := suite.ms.LoadLatestVersion()
+	require.NoError(t, err)
+
+	suite.ctx = sdk.NewContext(suite.ms, abci.Header{}, true, log.NewNopLogger())
+
+	suite.mkeeper = mkeeper.NewKeeper(app.MakeCodec(), mKey)
+	suite.dkeeper = keeper.NewKeeper(app.MakeCodec(), dKey)
+
+	suite.handler = handler.NewHandler(suite.dkeeper, suite.mkeeper)
+
+	return suite
+}
+
+func TestProviderBadMessageType(t *testing.T) {
+	suite := setupTestSuite(t)
+
+	res, err := suite.handler(suite.ctx, sdk.NewTestMsg())
+	require.Nil(t, res)
+	require.Error(t, err)
+	require.True(t, errors.Is(err, sdkerrors.ErrUnknownRequest))
+}
+
+func TestCreateDeployment(t *testing.T) {
+	suite := setupTestSuite(t)
+
+	deployment, groups := suite.createDeployment()
+
+	msg := types.MsgCreateDeployment{
+		ID:     deployment.ID(),
+		Groups: make([]types.GroupSpec, 0, len(groups)),
+	}
+
+	for _, group := range groups {
+		msg.Groups = append(msg.Groups, group.GroupSpec)
+	}
+
+	res, err := suite.handler(suite.ctx, msg)
+	require.NoError(t, err)
+	require.IsType(t, &sdk.Result{}, res)
+
+	_, exists := suite.dkeeper.GetDeployment(suite.ctx, deployment.ID())
+	require.True(t, exists)
+
+	res, err = suite.handler(suite.ctx, msg)
+	require.EqualError(t, err, types.ErrDeploymentExists.Error())
+	require.Nil(t, res)
+}
+
+func TestCreateDeploymentEmptyGroups(t *testing.T) {
+	suite := setupTestSuite(t)
+
+	deployment := testutil.Deployment(suite.t)
+
+	msg := types.MsgCreateDeployment{
+		ID: deployment.ID(),
+	}
+
+	res, err := suite.handler(suite.ctx, msg)
+	require.Nil(t, res)
+	require.True(t, errors.Is(err, types.ErrInvalidGroups))
+}
+
+func TestUpdateDeploymentNonExisting(t *testing.T) {
+	suite := setupTestSuite(t)
+
+	deployment := testutil.Deployment(suite.t)
+
+	msg := types.MsgUpdateDeployment{
+		ID: deployment.ID(),
+	}
+
+	res, err := suite.handler(suite.ctx, msg)
+	require.Nil(t, res)
+	require.EqualError(t, err, types.ErrDeploymentNotFound.Error())
+}
+
+func TestUpdateDeploymentExisting(t *testing.T) {
+	suite := setupTestSuite(t)
+
+	deployment, groups := suite.createDeployment()
+
+	msg := types.MsgCreateDeployment{
+		ID:     deployment.ID(),
+		Groups: make([]types.GroupSpec, 0, len(groups)),
+	}
+
+	for _, group := range groups {
+		msg.Groups = append(msg.Groups, group.GroupSpec)
+	}
+
+	res, err := suite.handler(suite.ctx, msg)
+	require.NoError(t, err)
+	require.IsType(t, &sdk.Result{}, res)
+
+	msgUpdate := types.MsgUpdateDeployment{
+		ID: deployment.ID(),
+	}
+
+	res, err = suite.handler(suite.ctx, msgUpdate)
+	require.NotNil(t, res)
+	require.NoError(t, err)
+}
+
+func TestCloseDeploymentNonExisting(t *testing.T) {
+	suite := setupTestSuite(t)
+
+	deployment := testutil.Deployment(suite.t)
+
+	msg := types.MsgCloseDeployment{
+		ID: deployment.ID(),
+	}
+
+	res, err := suite.handler(suite.ctx, msg)
+	require.Nil(t, res)
+	require.EqualError(t, err, types.ErrDeploymentNotFound.Error())
+}
+
+func TestCloseDeploymentExisting(t *testing.T) {
+	suite := setupTestSuite(t)
+
+	deployment, groups := suite.createDeployment()
+
+	msg := types.MsgCreateDeployment{
+		ID:     deployment.ID(),
+		Groups: make([]types.GroupSpec, 0, len(groups)),
+	}
+
+	for _, group := range groups {
+		msg.Groups = append(msg.Groups, group.GroupSpec)
+	}
+
+	res, err := suite.handler(suite.ctx, msg)
+	require.NoError(t, err)
+	require.IsType(t, &sdk.Result{}, res)
+
+	msgClose := types.MsgCloseDeployment{
+		ID: deployment.ID(),
+	}
+
+	res, err = suite.handler(suite.ctx, msgClose)
+	require.NotNil(t, res)
+	require.NoError(t, err)
+
+	res, err = suite.handler(suite.ctx, msgClose)
+	require.Nil(t, res)
+	require.EqualError(t, err, types.ErrDeploymentClosed.Error())
+}
+
+func (st *testSuite) createDeployment() (types.Deployment, []types.Group) {
+	st.t.Helper()
+
+	deployment := testutil.Deployment(st.t)
+	group := testutil.DeploymentGroup(st.t, deployment.ID(), 0)
+	group.Resources = []types.Resource{
+		{
+			Unit: atypes.Unit{
+				CPU:     100,
+				Memory:  100,
+				Storage: 10,
+			},
+			Count: 1,
+			Price: sdk.NewCoin("thepricedcoin", sdk.NewInt(int64(rand.Intn(10000)))),
+		},
+	}
+	groups := []types.Group{
+		group,
+	}
+
+	for i := range groups {
+		groups[i].State = types.GroupMatched
+	}
+
+	return deployment, groups
+}
+
+func (st *testSuite) createActiveDeployment() (types.Deployment, []types.Group) {
+	st.t.Helper()
+
+	deployment, groups := st.createDeployment()
+
+	err := st.dkeeper.Create(st.ctx, deployment, groups)
+	require.NoError(st.t, err)
+
+	return deployment, groups
+}
--- x/market/types/errors.go
+++ x/market/types/errors.go
@@ -4,39 +4,62 @@ import (
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
+const (
+	errCodeInvalidOrder uint32 = iota + 1
+	errCodeEmptyProvider
+	errCodeSameAccount
+	errCodeInternal
+	errCodeOverOrder
+	errCodeAttributeMismatch
+	errCodeUnknownBid
+	errCodeUnknownLeaseForOrder
+	errCodeUnknownOrderForBid
+	errCodeLeaseNotActive
+	errCodeBidNotMatched
+	errCodeUnknownOrder
+	errCodeNoLeaseForOrder
+	errCodeOrderNotFound
+	errCodeBidNotFound
+	errCodeLeaseNotFound
+	errCodeBidExists
+	errCodeInvalidPrice
+)
+
 var (
 	// ErrInvalidOrder is the error when order id is invalid
-	ErrInvalidOrder = sdkerrors.Register(ModuleName, 1, "invalid: order id")
+	ErrInvalidOrder = sdkerrors.Register(ModuleName, errCodeInvalidOrder, "invalid: order id")
 	// ErrEmptyProvider is the error when provider is empty
-	ErrEmptyProvider = sdkerrors.Register(ModuleName, 2, "empty provider")
+	ErrEmptyProvider = sdkerrors.Register(ModuleName, errCodeEmptyProvider, "empty provider")
 	// ErrSameAccount is the error when owner and provider are the same account
-	ErrSameAccount = sdkerrors.Register(ModuleName, 3, "owner and provider are the same account")
+	ErrSameAccount = sdkerrors.Register(ModuleName, errCodeSameAccount, "owner and provider are the same account")
 	// ErrInternal is the error for internal error
-	ErrInternal = sdkerrors.Register(ModuleName, 4, "internal error")
+	ErrInternal = sdkerrors.Register(ModuleName, errCodeInternal, "internal error")
 	// ErrBidOverOrder is the error when bid price is above max order price
-	ErrBidOverOrder = sdkerrors.Register(ModuleName, 5, "bid price above max order price")
-	// ErrAtributeMismatch is the error for attribute mismatch
-	ErrAtributeMismatch = sdkerrors.Register(ModuleName, 6, "attribute mismatch")
+	ErrBidOverOrder = sdkerrors.Register(ModuleName, errCodeOverOrder, "bid price above max order price")
+	// ErrAttributeMismatch is the error for attribute mismatch
+	ErrAttributeMismatch = sdkerrors.Register(ModuleName, errCodeAttributeMismatch, "attribute mismatch")
 	// ErrUnknownBid is the error for unknown bid
-	ErrUnknownBid = sdkerrors.Register(ModuleName, 7, "unknown bid")
+	ErrUnknownBid = sdkerrors.Register(ModuleName, errCodeUnknownBid, "unknown bid")
 	// ErrUnknownLeaseForBid is the error when lease is unknown for bid
-	ErrUnknownLeaseForBid = sdkerrors.Register(ModuleName, 8, "unknown lease for bid")
+	ErrUnknownLeaseForBid = sdkerrors.Register(ModuleName, errCodeUnknownLeaseForOrder, "unknown lease for bid")
 	// ErrUnknownOrderForBid is the error when order is unknown for bid
-	ErrUnknownOrderForBid = sdkerrors.Register(ModuleName, 9, "unknown order for bid")
+	ErrUnknownOrderForBid = sdkerrors.Register(ModuleName, errCodeUnknownOrderForBid, "unknown order for bid")
 	// ErrLeaseNotActive is the error when lease is not active
-	ErrLeaseNotActive = sdkerrors.Register(ModuleName, 10, "lease not active")
+	ErrLeaseNotActive = sdkerrors.Register(ModuleName, errCodeLeaseNotActive, "lease not active")
 	// ErrBidNotMatched is the error when bid is not matched
-	ErrBidNotMatched = sdkerrors.Register(ModuleName, 11, "bid not matched")
+	ErrBidNotMatched = sdkerrors.Register(ModuleName, errCodeBidNotMatched, "bid not matched")
 	// ErrUnknownOrder is the error when order is unknown
-	ErrUnknownOrder = sdkerrors.Register(ModuleName, 12, "unknown order")
+	ErrUnknownOrder = sdkerrors.Register(ModuleName, errCodeUnknownOrder, "unknown order")
 	// ErrNoLeaseForOrder is the error when there is no lease for order
-	ErrNoLeaseForOrder = sdkerrors.Register(ModuleName, 13, "no lease for order")
+	ErrNoLeaseForOrder = sdkerrors.Register(ModuleName, errCodeNoLeaseForOrder, "no lease for order")
 	// ErrOrderNotFound order not found
-	ErrOrderNotFound = sdkerrors.Register(ModuleName, 14, "invalid order: order not found")
+	ErrOrderNotFound = sdkerrors.Register(ModuleName, errCodeOrderNotFound, "invalid order: order not found")
 	// ErrBidNotFound bid not found
-	ErrBidNotFound = sdkerrors.Register(ModuleName, 15, "invalid bid: bid not found")
+	ErrBidNotFound = sdkerrors.Register(ModuleName, errCodeBidNotFound, "invalid bid: bid not found")
 	// ErrLeaseNotFound lease not found
-	ErrLeaseNotFound = sdkerrors.Register(ModuleName, 16, "invalid lease: lease not found")
+	ErrLeaseNotFound = sdkerrors.Register(ModuleName, errCodeLeaseNotFound, "invalid lease: lease not found")
 	// ErrBidExists bid exists
-	ErrBidExists = sdkerrors.Register(ModuleName, 17, "invalid bid: bid exists from provider")
+	ErrBidExists = sdkerrors.Register(ModuleName, errCodeBidExists, "invalid bid: bid exists from provider")
+	// ErrBidInvalidPrice bid invalid price
+	ErrBidInvalidPrice = sdkerrors.Register(ModuleName, errCodeInvalidPrice, "bid price is invalid")
 )
