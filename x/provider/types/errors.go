de4450a9db85afe787e6b0d01235d70ad825370f
Artur Troian <troian@users.noreply.github.com>
2020-06-15 12:07:14 -0400

    refactor: move immediate code declarations to constans (#667)

--- x/provider/types/errors.go
+++ x/provider/types/errors.go
@@ -4,22 +4,31 @@ import (
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
+const (
+	errInvalidProviderURI uint32 = iota + 1
+	errNotAbsProviderURI
+	errProviderNotFound
+	errProviderExists
+	errInvalidAddress
+	errAttributes
+)
+
 var (
 	// ErrInvalidProviderURI register error code for invalid provider uri
-	ErrInvalidProviderURI = sdkerrors.Register(ModuleName, 1, "invalid provider: invalid host uri")
+	ErrInvalidProviderURI = sdkerrors.Register(ModuleName, errInvalidProviderURI, "invalid provider: invalid host uri")
 
 	// ErrNotAbsProviderURI register error code for not absolute provider uri
-	ErrNotAbsProviderURI = sdkerrors.Register(ModuleName, 2, "invalid provider: not absolute host uri")
+	ErrNotAbsProviderURI = sdkerrors.Register(ModuleName, errNotAbsProviderURI, "invalid provider: not absolute host uri")
 
 	// ErrProviderNotFound provider not found
-	ErrProviderNotFound = sdkerrors.Register(ModuleName, 3, "invalid provider: address not found")
+	ErrProviderNotFound = sdkerrors.Register(ModuleName, errProviderNotFound, "invalid provider: address not found")
 
 	// ErrProviderExists provider already exists
-	ErrProviderExists = sdkerrors.Register(ModuleName, 6, "invalid provider: already exists")
+	ErrProviderExists = sdkerrors.Register(ModuleName, errProviderExists, "invalid provider: already exists")
 
 	// ErrInvalidAddress invalid provider address
-	ErrInvalidAddress = sdkerrors.Register(ModuleName, 4, "invalid address")
+	ErrInvalidAddress = sdkerrors.Register(ModuleName, errInvalidAddress, "invalid address")
 
 	// ErrAttributes error code for provider attribute problems
-	ErrAttributes = sdkerrors.Register(ModuleName, 5, "attribute specification error")
+	ErrAttributes = sdkerrors.Register(ModuleName, errAttributes, "attribute specification error")
 )
