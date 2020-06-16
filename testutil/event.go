18ec2d2708ca3b3c127b846e294ef357d0743bb1
akhilkumarpilli <akhilkumar7947@gmail.com>
2020-06-16 21:36:06 +0530

    Add one ensure event test in market

--- testutil/event.go
+++ testutil/event.go
@@ -6,15 +6,16 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ovrclk/akash/sdkutil"
 	dtypes "github.com/ovrclk/akash/x/deployment/types"
+	mtypes "github.com/ovrclk/akash/x/market/types"
 	"github.com/stretchr/testify/require"
 )
 
-func ParseEvent(t testing.TB, events sdk.Events) sdkutil.Event {
+func ParseEvent(t testing.TB, events sdk.Events, expectedLen int) sdkutil.Event {
 	t.Helper()
 
-	require.Equal(t, 1, len(events))
+	require.Equal(t, expectedLen, len(events))
 
-	sev := sdk.StringifyEvent(events.ToABCIEvents()[0])
+	sev := sdk.StringifyEvent(events.ToABCIEvents()[expectedLen-1])
 	ev, err := sdkutil.ParseEvent(sev)
 
 	require.NoError(t, err)
@@ -25,9 +26,20 @@ func ParseEvent(t testing.TB, events sdk.Events) sdkutil.Event {
 func ParseDeploymentEvent(t testing.TB, events sdk.Events) sdkutil.ModuleEvent {
 	t.Helper()
 
-	uev := ParseEvent(t, events)
+	uev := ParseEvent(t, events, 1)
 
 	iev, err := dtypes.ParseEvent(uev)
+	require.NoError(t, err)
+
+	return iev
+}
+
+func ParseMarketEvent(t testing.TB, events sdk.Events, expectedLen int) sdkutil.ModuleEvent {
+	t.Helper()
+
+	uev := ParseEvent(t, events, expectedLen)
+
+	iev, err := mtypes.ParseEvent(uev)
 	require.NoError(t, err)
 
 	return iev
