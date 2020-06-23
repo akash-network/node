24076032ab2abce6addc82dff97a2ddfe04b079b
akhilkumarpilli <akhilkumar7947@gmail.com>
2020-06-23 16:19:48 +0530

    Format cmd configure file

--- cmd/akashctl/configure.go
+++ cmd/akashctl/configure.go
@@ -2,14 +2,14 @@
 
 package main
 
-import(
+import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	ecmd "github.com/ovrclk/akash/events/cmd"
 	pcmd "github.com/ovrclk/akash/provider/cmd"
 	"github.com/spf13/cobra"
 )
 
-func addOtherCommands(root *cobra.Command, cdc *codec.Codec){
+func addOtherCommands(root *cobra.Command, cdc *codec.Codec) {
 	root.AddCommand(
 		pcmd.RootCmd(cdc),
 		ecmd.EventCmd(cdc),
