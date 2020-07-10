53756b1336655f3d81ba245890258770c77dcb91
Jack Zampolin <jack.zampolin@gmail.com>
2020-07-10 10:46:46 -0700

    WIP command to listen for lease creation and send manifests to providers

--- x/deployment/client/cli/tx.go
+++ x/deployment/client/cli/tx.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	ccontext "context"
 	"fmt"
 	"os"
 
@@ -8,12 +9,19 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"golang.org/x/sync/errgroup"
 
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/x/auth"
 	"github.com/cosmos/cosmos-sdk/x/auth/client/utils"
+	"github.com/ovrclk/akash/events"
+	"github.com/ovrclk/akash/provider/gateway"
+	"github.com/ovrclk/akash/provider/manifest"
+	"github.com/ovrclk/akash/pubsub"
 	"github.com/ovrclk/akash/sdl"
 	"github.com/ovrclk/akash/x/deployment/types"
+	mtypes "github.com/ovrclk/akash/x/market/types"
+	pmodule "github.com/ovrclk/akash/x/provider"
 
 	"github.com/spf13/cobra"
 )
@@ -32,7 +40,146 @@ func GetTxCmd(key string, cdc *codec.Codec) *cobra.Command {
 		cmdUpdate(key, cdc),
 		cmdClose(key, cdc),
 		cmdGroupClose(key, cdc),
+		makeItSoCommand(key, cdc),
 	)...)
+	return cmd
+}
+
+func makeItSoCommand(key string, cdc *codec.Codec) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "make-it-so [sdl-file]",
+		Short: fmt.Sprintf("Create %s", key),
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			// Setup CLI context and transaction builder
+			ctx := context.NewCLIContext().WithCodec(cdc)
+			bldr := auth.NewTxBuilderFromCLI(os.Stdin).WithTxEncoder(utils.GetTxEncoder(cdc))
+
+			// Read in manifest file
+			sdl, err := sdl.ReadFile(args[0])
+			if err != nil {
+				return err
+			}
+
+			// Fetch deployment groups from the SDL file, each group maps to an individual order
+			groups, err := sdl.DeploymentGroups()
+			if err != nil {
+				return err
+			}
+
+			// Fetch the manifest from the SDL file
+			mani, err := sdl.Manifest()
+			if err != nil {
+				return err
+			}
+
+			// Get the deployment ID (defaults to height)
+			id, err := DeploymentIDFromFlags(cmd.Flags(), ctx.GetFromAddress().String())
+			if err != nil {
+				return err
+			}
+
+			// Default DSeq to the current block height
+			if id.DSeq == 0 {
+				if id.DSeq, err = currentBlockHeight(ctx); err != nil {
+					return err
+				}
+			}
+
+			// create a new pubsub bus to handle chain events
+			bus := pubsub.NewBus()
+			defer bus.Close()
+
+			// Create an error group to handle chain listener
+			group, cctx := errgroup.WithContext(ccontext.Background())
+
+			group.Go(func() error {
+				// Listen to the events on chain and publish them to the pubsub bus
+				return events.Publish(cctx, ctx.Client, "deployment-create", bus)
+			})
+
+			// expose event channel
+			sub, err := bus.Subscribe()
+			if err != nil {
+				return err
+			}
+
+			// start goroutine to listen for events
+			leases := len(groups)
+			group.Go(func() error {
+				fmt.Println("Starting event listener")
+				for {
+					select {
+					case <-sub.Done():
+						return nil
+					case ev := <-sub.Events():
+						fmt.Println("getting event")
+						switch msg := ev.(type) {
+						case types.EventDeploymentCreated:
+							fmt.Printf("Deployment %d created...\n", msg.ID)
+						case mtypes.EventOrderCreated:
+							fmt.Printf("Order %d for deployement %d created...\n", msg.ID.OSeq, msg.ID.DSeq)
+						case mtypes.EventBidCreated:
+							fmt.Printf("Bid of %s for order %d:%d created...\n", msg.Price, msg.ID.DSeq, msg.ID.OSeq)
+						case mtypes.EventLeaseCreated:
+							fmt.Printf("Lease for order %d:%d created...\n", msg.ID.DSeq, msg.ID.OSeq)
+							pclient := pmodule.AppModuleBasic{}.GetQueryClient(ctx)
+							provider, err := pclient.Provider(msg.ID.Provider)
+							if err != nil {
+								return err
+							}
+
+							gclient := gateway.NewClient()
+
+							fmt.Printf("Sending manifest to provider %s...\n", msg.ID.Provider)
+							if err = gclient.SubmitManifest(
+								ccontext.Background(),
+								provider.HostURI,
+								&manifest.SubmitRequest{
+									Deployment: msg.ID.DeploymentID(),
+									Manifest:   mani,
+								},
+							); err != nil {
+								return err
+							}
+
+							if leases += -1; leases == 0 {
+								sub.Close()
+							}
+						}
+					}
+				}
+			})
+
+			// Create the deployment message
+			msg := types.MsgCreateDeployment{
+				ID: id,
+				// Version:  []byte{0x1, 0x2},
+				Groups: make([]types.GroupSpec, 0, len(groups)),
+			}
+
+			// Append the groups to the message
+			for _, group := range groups {
+				msg.Groups = append(msg.Groups, *group)
+			}
+
+			// validate
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+
+			// broadcast the message
+			// TODO: set transaction broadcast defaults on the ctx to ensure
+			// that GenerateOrBroadcastMsgs exits when tx is sent to chain
+			if err = utils.GenerateOrBroadcastMsgs(ctx, bldr, []sdk.Msg{msg}); err != nil {
+				return err
+			}
+
+			return group.Wait()
+		},
+	}
+	AddDeploymentIDFlags(cmd.Flags())
+
 	return cmd
 }
 
