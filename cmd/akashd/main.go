0ac0fe28a24fc40638916aa7e62bfbea4cd437eb
Alessio Treglia <alessio@tendermint.com>
2020-07-14 20:09:50 +0100

    Port pruning options handling

--- cmd/akashd/main.go
+++ cmd/akashd/main.go
@@ -88,9 +88,14 @@ func newApp(logger log.Logger, db dbm.DB, tio io.Writer) abci.Application {
 		skipUpgradeHeights[int64(h)] = true
 	}
 
+	pruningOpts, err := server.GetPruningOptionsFromFlags()
+	if err != nil {
+		panic(err)
+	}
+
 	return app.NewApp(
 		logger, db, tio, invCheckPeriod, skipUpgradeHeights,
-		baseapp.SetPruning(store.NewPruningOptionsFromString(viper.GetString("pruning"))),
+		baseapp.SetPruning(pruningOpts),
 		baseapp.SetMinGasPrices(viper.GetString(server.FlagMinGasPrices)),
 		baseapp.SetHaltHeight(viper.GetUint64(server.FlagHaltHeight)),
 		baseapp.SetHaltTime(viper.GetUint64(server.FlagHaltTime)),
