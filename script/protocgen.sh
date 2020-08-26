c702b747c8c6ef37df59b6ed56cc1c9780c5bd4f
Jack Zampolin <jack.zampolin@gmail.com>
2020-08-26 13:07:27 -0700

    Shellcheck passing

--- script/protocgen.sh
+++ script/protocgen.sh
@@ -3,6 +3,7 @@
 set -eo pipefail
 
 proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+#shellcheck disable=SC2046
 for dir in $proto_dirs; do
   protoc \
   -I "proto" \
