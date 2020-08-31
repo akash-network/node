f25e4a32a321d84b3e354019863ef4e51b3ab2ce
Artur Troian <troian.ap@gmail.com>
2020-08-31 19:40:22 -0400

    feat: install protobuf tools into local cache
    fixes #796
    
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- script/protocgen.sh
+++ script/protocgen.sh
@@ -5,9 +5,10 @@ set -eo pipefail
 proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 #shellcheck disable=SC2046
 for dir in $proto_dirs; do
-  protoc \
+  .cache/bin/protoc \
   -I "proto" \
   -I "third_party/proto" \
+  -I ".cache/include" \
   --gocosmos_out=plugins=interfacetype+grpc,\
 Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
   $(find "${dir}" -maxdepth 1 -name '*.proto')
