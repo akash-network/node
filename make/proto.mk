6ced9b3b4def5d3a7215255081214a47293bf471
Artur Troian <troian.ap@gmail.com>
2020-10-07 11:23:08 -0400

    refactor: format make files
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- make/proto.mk
+++ make/proto.mk
@@ -2,12 +2,12 @@
 ###                           Protobuf                                    ###
 ###############################################################################
 ifeq ($(UNAME_OS),Linux)
-  PROTOC_ZIP ?= protoc-${PROTOC_VERSION}-linux-x86_64.zip
-  CLANG_FORMAT_BIN ?= $(shell [ -f /etc/debian_version ] && echo "clang-format-6.0" || echo "clang-format")
+	PROTOC_ZIP       ?= protoc-${PROTOC_VERSION}-linux-x86_64.zip
+	CLANG_FORMAT_BIN ?= $(shell [ -f /etc/debian_version ] && echo "clang-format-6.0" || echo "clang-format")
 endif
 ifeq ($(UNAME_OS),Darwin)
-  PROTOC_ZIP ?= protoc-${PROTOC_VERSION}-osx-x86_64.zip
-  CLANG_FORMAT_BIN ?= clang-format
+	PROTOC_ZIP       ?= protoc-${PROTOC_VERSION}-osx-x86_64.zip
+	CLANG_FORMAT_BIN ?= clang-format
 endif
 
 proto-lint: $(BUF)
