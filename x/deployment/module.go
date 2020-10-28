0273c93ad6bec1974160dbafffb4bfb94ef0ec26
Jack Zampolin <jack.zampolin@gmail.com>
2020-10-28 12:57:38 -0700

    Update to latest master

--- x/deployment/module.go
+++ x/deployment/module.go
@@ -84,8 +84,8 @@ func (AppModuleBasic) RegisterRESTRoutes(clientCtx client.Context, rtr *mux.Rout
 	rest.RegisterRoutes(clientCtx, rtr, StoreKey)
 }
 
-// RegisterGRPCRoutes registers the gRPC Gateway routes for the deployment module.
-func (AppModuleBasic) RegisterGRPCRoutes(clientCtx client.Context, mux *runtime.ServeMux) {
+// RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the deployment module.
+func (AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *runtime.ServeMux) {
 	err := types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(clientCtx))
 	if err != nil {
 		panic(fmt.Sprintf("couldn't register deployment grpc routes: %s", err.Error()))
