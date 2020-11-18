ca6c4850bebfe9a58ad6f7ba5a310fcec61818f3
Eric Urban <hydrogen18@gmail.com>
2020-11-18 17:24:23 -0600

    test: fix some tests

--- testutil/sdk.go
+++ testutil/sdk.go
@@ -13,6 +13,13 @@ func Coin(t testing.TB) sdk.Coin {
 }
 
 // AkashCoin provides simple interface to the Akash sdk.Coin type.
+func AkashCoinRandom(t testing.TB) sdk.Coin {
+  t.Helper()
+  amt := sdk.NewInt(int64(rand.Intn(999)+1))
+  return sdk.NewCoin(CoinDenom, amt)
+}
+
+// AkashCoin provides simple interface to the Akash sdk.Coin type.
 func AkashCoin(t testing.TB, amount int64) sdk.Coin {
 	t.Helper()
 	amt := sdk.NewInt(amount)
