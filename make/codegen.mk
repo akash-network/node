forbole/akash

build: automate installation of dev dependencies
each dependency is being installed to .cache uppon
it's necessity
install k8s.io to .cache
install kine to .cache
fetch grpc-gateway version from go mod

Signed-off-by: Artur Troian <troian.ap@gmail.com>

make/codegen.mk
@@ -21,9 +21,8 @@ mocks: $(MOCKERY) modvendor
 	$(MOCKERY) --case=underscore --dir x/escrow/keeper             --output x/escrow/keeper/mocks       --name BankKeeper
 
 .PHONY: kubetypes
-kubetypes: deps-vendor
-	chmod +x vendor/k8s.io/code-generator/generate-groups.sh
-	vendor/k8s.io/code-generator/generate-groups.sh all \
+kubetypes: $(K8S_GENERATE_GROUPS)
+	GOBIN=$(CACHE_BIN) $(K8S_GENERATE_GROUPS) all \
 	github.com/ovrclk/akash/pkg/client github.com/ovrclk/akash/pkg/apis \
 	akash.network:v1
 

akash-network/node

make/codegen.mk
@@ -21,9 +21,8 @@ mocks: $(MOCKERY) modvendor
 	$(MOCKERY) --case=underscore --dir x/escrow/keeper             --output x/escrow/keeper/mocks       --name BankKeeper
 
 .PHONY: kubetypes
-kubetypes: deps-vendor
-	chmod +x vendor/k8s.io/code-generator/generate-groups.sh
-	vendor/k8s.io/code-generator/generate-groups.sh all \
+kubetypes: $(K8S_GENERATE_GROUPS)
+	GOBIN=$(AKASH_DEVCACHE_BIN) $(K8S_GENERATE_GROUPS) all \
 	github.com/ovrclk/akash/pkg/client github.com/ovrclk/akash/pkg/apis \
 	akash.network:v1
 