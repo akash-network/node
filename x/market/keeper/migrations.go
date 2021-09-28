5ad0a05e6cbbd9e53b8f3b3d48616bee6457247b
Eric Urban <hydrogen18@gmail.com>
2021-09-28 15:41:32 -0500

    WIP

--- x/market/keeper/migrations.go
+++ x/market/keeper/migrations.go
@@ -3,6 +3,7 @@ package keeper
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	v013 "github.com/ovrclk/akash/x/market/legacy/v013"
+	v014 "github.com/ovrclk/akash/x/market/legacy/v014"
 )
 
 // Migrator is a struct for handling in-place store migrations.
@@ -18,4 +19,8 @@ func NewMigrator(k IKeeper) Migrator {
 // Migrate1to2 migrates from version 1 to 2.
 func (m Migrator) Migrate1to2(ctx sdk.Context) error {
 	return v013.MigrateStore(ctx, m.keeper.skey)
+}
+
+func (m Migrator) MigrateLease(ctx sdk.Context) error {
+	return v014.MigrateStore(ctx, m.keeper.skey)
 }
