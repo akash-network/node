xmonader/akash

feat(provider): add IP address marketplace based off metal LB

pkg/client/clientset/versioned/typed/akash.network/v1/akash.network_client.go
@@ -28,6 +28,7 @@ type AkashV1Interface interface {
 	RESTClient() rest.Interface
 	ManifestsGetter
 	ProviderHostsGetter
+	ProviderLeasedIPsGetter
 }
 
 // AkashV1Client is used to interact with features provided by the akash.network group.
@@ -41,6 +42,10 @@ func (c *AkashV1Client) Manifests(namespace string) ManifestInterface {
 
 func (c *AkashV1Client) ProviderHosts(namespace string) ProviderHostInterface {
 	return newProviderHosts(c, namespace)
+}
+
+func (c *AkashV1Client) ProviderLeasedIPs(namespace string) ProviderLeasedIPInterface {
+	return newProviderLeasedIPs(c, namespace)
 }
 
 // NewForConfig creates a new AkashV1Client for the given config.

provider/cluster/mocks/client.go
@@ -64,6 +64,20 @@ func (_m *Client) ConnectHostnameToDeployment(ctx context.Context, directive v1b
 	return r0
 }
 
+// CreateIPPassthrough provides a mock function with given fields: ctx, lID, directive
+func (_m *Client) CreateIPPassthrough(ctx context.Context, lID v1beta2.LeaseID, directive cluster.ClusterIPPassthroughDirective) error {
+	ret := _m.Called(ctx, lID, directive)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, v1beta2.LeaseID, cluster.ClusterIPPassthroughDirective) error); ok {
+		r0 = rf(ctx, lID, directive)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
 // DeclareHostname provides a mock function with given fields: ctx, lID, host, serviceName, externalPort
 func (_m *Client) DeclareHostname(ctx context.Context, lID typesv1beta2.LeaseID, host string, serviceName string, externalPort uint32) error {
 	ret := _m.Called(ctx, lID, host, serviceName, externalPort)
@@ -71,6 +85,20 @@ func (_m *Client) DeclareHostname(ctx context.Context, lID typesv1beta2.LeaseID,
 	var r0 error
 	if rf, ok := ret.Get(0).(func(context.Context, typesv1beta2.LeaseID, string, string, uint32) error); ok {
 		r0 = rf(ctx, lID, host, serviceName, externalPort)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// DeclareIP provides a mock function with given fields: ctx, lID, serviceName, port, externalPort, proto, sharingKey
+func (_m *Client) DeclareIP(ctx context.Context, lID v1beta2.LeaseID, serviceName string, port uint32, externalPort uint32, proto manifest.ServiceProtocol, sharingKey string) error {
+	ret := _m.Called(ctx, lID, serviceName, port, externalPort, proto, sharingKey)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, v1beta2.LeaseID, string, uint32, uint32, manifest.ServiceProtocol, string) error); ok {
+		r0 = rf(ctx, lID, serviceName, port, externalPort, proto, sharingKey)
 	} else {
 		r0 = ret.Error(0)
 	}
@@ -148,6 +176,29 @@ func (_m *Client) GetHostnameDeploymentConnections(ctx context.Context) ([]v1bet
 	} else {
 		if ret.Get(0) != nil {
 			r0 = ret.Get(0).([]v1beta2.LeaseIDHostnameConnection)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context) error); ok {
+		r1 = rf(ctx)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// GetIPPassthroughs provides a mock function with given fields: ctx
+func (_m *Client) GetIPPassthroughs(ctx context.Context) ([]cluster.IPPassthrough, error) {
+	ret := _m.Called(ctx)
+
+	var r0 []cluster.IPPassthrough
+	if rf, ok := ret.Get(0).(func(context.Context) []cluster.IPPassthrough); ok {
+		r0 = rf(ctx)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]cluster.IPPassthrough)
 		}
 	}
 
@@ -327,6 +378,29 @@ func (_m *Client) ObserveHostnameState(ctx context.Context) (<-chan v1beta2.Host
 	return r0, r1
 }
 
+// ObserveIPState provides a mock function with given fields: ctx
+func (_m *Client) ObserveIPState(ctx context.Context) (<-chan cluster.IPResourceEvent, error) {
+	ret := _m.Called(ctx)
+
+	var r0 <-chan cluster.IPResourceEvent
+	if rf, ok := ret.Get(0).(func(context.Context) <-chan cluster.IPResourceEvent); ok {
+		r0 = rf(ctx)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(<-chan cluster.IPResourceEvent)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context) error); ok {
+		r1 = rf(ctx)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // PurgeDeclaredHostname provides a mock function with given fields: ctx, lID, hostname
 func (_m *Client) PurgeDeclaredHostname(ctx context.Context, lID typesv1beta2.LeaseID, hostname string) error {
 	ret := _m.Called(ctx, lID, hostname)
@@ -348,6 +422,48 @@ func (_m *Client) PurgeDeclaredHostnames(ctx context.Context, lID typesv1beta2.L
 	var r0 error
 	if rf, ok := ret.Get(0).(func(context.Context, typesv1beta2.LeaseID) error); ok {
 		r0 = rf(ctx, lID)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// PurgeDeclaredIP provides a mock function with given fields: ctx, lID, serviceName, externalPort, proto
+func (_m *Client) PurgeDeclaredIP(ctx context.Context, lID v1beta2.LeaseID, serviceName string, externalPort uint32, proto manifest.ServiceProtocol) error {
+	ret := _m.Called(ctx, lID, serviceName, externalPort, proto)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, v1beta2.LeaseID, string, uint32, manifest.ServiceProtocol) error); ok {
+		r0 = rf(ctx, lID, serviceName, externalPort, proto)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// PurgeDeclaredIPs provides a mock function with given fields: ctx, lID
+func (_m *Client) PurgeDeclaredIPs(ctx context.Context, lID v1beta2.LeaseID) error {
+	ret := _m.Called(ctx, lID)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, v1beta2.LeaseID) error); ok {
+		r0 = rf(ctx, lID)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// PurgeIPPassthrough provides a mock function with given fields: ctx, lID, directive
+func (_m *Client) PurgeIPPassthrough(ctx context.Context, lID v1beta2.LeaseID, directive cluster.ClusterIPPassthroughDirective) error {
+	ret := _m.Called(ctx, lID, directive)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, v1beta2.LeaseID, cluster.ClusterIPPassthroughDirective) error); ok {
+		r0 = rf(ctx, lID, directive)
 	} else {
 		r0 = ret.Error(0)
 	}

provider/cluster/service.go
@@ -2,6 +2,8 @@ package cluster
 
 import (
 	"context"
+	"github.com/ovrclk/akash/provider/cluster/operator_clients"
+	"github.com/ovrclk/akash/provider/operator/waiter"
 
 	"github.com/boz/go-lifecycle"
 	sdktypes "github.com/cosmos/cosmos-sdk/types"
@@ -59,7 +61,7 @@ type Service interface {
 }
 
 // NewService returns new Service instance
-func NewService(ctx context.Context, session session.Session, bus pubsub.Bus, client Client, cfg Config) (Service, error) {
+func NewService(ctx context.Context, session session.Session, bus pubsub.Bus, client Client, ipOperatorClient operator_clients.IPOperatorClient, waiter waiter.OperatorWaiter, cfg Config) (Service, error) {
 	log := session.Log().With("module", "provider-cluster", "cmp", "service")
 
 	lc := lifecycle.New()
@@ -75,7 +77,8 @@ func NewService(ctx context.Context, session session.Session, bus pubsub.Bus, cl
 		return nil, err
 	}
 
-	inventory, err := newInventoryService(cfg, log, lc.ShuttingDown(), sub, client, deployments)
+
+	inventory, err := newInventoryService(cfg, log, lc.ShuttingDown(), sub, client, ipOperatorClient, waiter, deployments)
 	if err != nil {
 		sub.Close()
 		return nil, err
@@ -114,10 +117,11 @@ func NewService(ctx context.Context, session session.Session, bus pubsub.Bus, cl
 		log:    log,
 		lc:     lc,
 		config: cfg,
+		waiter: waiter,
 	}
 
 	go s.lc.WatchContext(ctx)
-	go s.run(deployments)
+	go s.run(ctx, deployments)
 
 	return s, nil
 }
@@ -139,6 +143,8 @@ type service struct {
 
 	log log.Logger
 	lc  lifecycle.Lifecycle
+
+	waiter waiter.OperatorWaiter
 
 	config Config
 }
@@ -251,11 +257,19 @@ func (s *service) updateDeploymentManagerGauge() {
 	deploymentManagerGauge.Set(float64(len(s.managers)))
 }
 
-func (s *service) run(deployments []ctypes.Deployment) {
+func (s *service) run(ctx context.Context, deployments []ctypes.Deployment) {
 	defer s.lc.ShutdownCompleted()
 	defer s.sub.Close()
 
 	s.updateDeploymentManagerGauge()
+
+	// wait for configured operators to be online & responsive before proceeding
+	err := s.waiter.WaitForAll(ctx)
+	if err != nil {
+		s.lc.ShutdownInitiated(err)
+		return
+	}
+
 	for _, deployment := range deployments {
 		key := deployment.LeaseID()
 		mgroup := deployment.ManifestGroup()
@@ -272,7 +286,7 @@ loop:
 		case ev := <-s.sub.Events():
 			switch ev := ev.(type) {
 			case event.ManifestReceived:
-				s.log.Info("manifest received")
+				s.log.Info("manifest received", "lease", ev.LeaseID)
 
 				mgroup := ev.ManifestGroup()
 				if mgroup == nil {

provider/service.go
@@ -2,15 +2,15 @@ package provider
 
 import (
 	"context"
-	"time"
-
 	clustertypes "github.com/ovrclk/akash/provider/cluster/types/v1beta2"
+
+	"github.com/boz/go-lifecycle"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	bankTypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-
-	"github.com/boz/go-lifecycle"
+	"github.com/ovrclk/akash/provider/cluster/operator_clients"
+	"github.com/ovrclk/akash/provider/operator/waiter"
 	"github.com/pkg/errors"
 
 	"github.com/ovrclk/akash/provider/bidengine"
@@ -21,9 +21,6 @@ import (
 	dtypes "github.com/ovrclk/akash/x/deployment/types/v1beta2"
 )
 
-var (
-	ErrClusterReadTimedout = errors.New("timeout waiting for cluster ready")
-)
 
 // ValidateClient is the interface to check if provider will bid on given groupspec
 type ValidateClient interface {
@@ -56,7 +53,15 @@ type Service interface {
 // NewService creates and returns new Service instance
 // Simple wrapper around various services needed for running a provider.
 
-func NewService(ctx context.Context, cctx client.Context, accAddr sdk.AccAddress, session session.Session, bus pubsub.Bus, cclient cluster.Client, cfg Config) (Service, error) {
+func NewService(ctx context.Context,
+	cctx client.Context,
+	accAddr sdk.AccAddress,
+	session session.Session,
+	bus pubsub.Bus,
+	cclient cluster.Client,
+	ipOperatorClient operator_clients.IPOperatorClient,
+	waiter waiter.OperatorWaiter,
+	cfg Config) (Service, error) {
 	ctx, cancel := context.WithCancel(ctx)
 
 	session = session.ForModule("provider-service")
@@ -73,22 +78,13 @@ func NewService(ctx context.Context, cctx client.Context, accAddr sdk.AccAddress
 	clusterConfig.DeploymentIngressDomain = cfg.DeploymentIngressDomain
 	clusterConfig.ClusterSettings = cfg.ClusterSettings
 
-	cluster, err := cluster.NewService(ctx, session, bus, cclient, clusterConfig)
+	cluster, err := cluster.NewService(ctx, session, bus, cclient, ipOperatorClient, waiter, clusterConfig)
 	if err != nil {
 		cancel()
 		return nil, err
 	}
 
-	select {
-	case <-cluster.Ready():
-	case <-time.After(cfg.ClusterWaitReadyDuration):
-		session.Log().Error(ErrClusterReadTimedout.Error())
-		cancel()
-		<-cluster.Done()
-		return nil, ErrClusterReadTimedout
-	}
-
-	bidengine, err := bidengine.NewService(ctx, session, cluster, bus, bidengine.Config{
+	bidengine, err := bidengine.NewService(ctx, session, cluster, bus, waiter, bidengine.Config{
 		PricingStrategy: cfg.BidPricingStrategy,
 		Deposit:         cfg.BidDeposit,
 		BidTimeout:      cfg.BidTimeout,

akash-network/node

pkg/client/clientset/versioned/typed/akash.network/v2beta1/akash.network_client.go
@@ -30,6 +30,7 @@ type AkashV2beta1Interface interface {
 	InventoryRequestsGetter
 	ManifestsGetter
 	ProviderHostsGetter
+	ProviderLeasedIPsGetter
 }
 
 // AkashV2beta1Client is used to interact with features provided by the akash.network group.
@@ -51,6 +52,10 @@ func (c *AkashV2beta1Client) Manifests(namespace string) ManifestInterface {
 
 func (c *AkashV2beta1Client) ProviderHosts(namespace string) ProviderHostInterface {
 	return newProviderHosts(c, namespace)
+}
+
+func (c *AkashV2beta1Client) ProviderLeasedIPs(namespace string) ProviderLeasedIPInterface {
+	return newProviderLeasedIPs(c, namespace)
 }
 
 // NewForConfig creates a new AkashV2beta1Client for the given config.

provider/cluster/mocks/client.go
@@ -78,6 +78,20 @@ func (_m *Client) DeclareHostname(ctx context.Context, lID typesv1beta2.LeaseID,
 	return r0
 }
 
+// DeclareIP provides a mock function with given fields: ctx, lID, serviceName, port, externalPort, proto, sharingKey
+func (_m *Client) DeclareIP(ctx context.Context, lID typesv1beta2.LeaseID, serviceName string, port uint32, externalPort uint32, proto v2beta1.ServiceProtocol, sharingKey string) error {
+	ret := _m.Called(ctx, lID, serviceName, port, externalPort, proto, sharingKey)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, typesv1beta2.LeaseID, string, uint32, uint32, v2beta1.ServiceProtocol, string) error); ok {
+		r0 = rf(ctx, lID, serviceName, port, externalPort, proto, sharingKey)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
 // Deploy provides a mock function with given fields: ctx, lID, mgroup
 func (_m *Client) Deploy(ctx context.Context, lID typesv1beta2.LeaseID, mgroup *v2beta1.Group) error {
 	ret := _m.Called(ctx, lID, mgroup)
@@ -131,6 +145,29 @@ func (_m *Client) Exec(ctx context.Context, lID typesv1beta2.LeaseID, service st
 	var r1 error
 	if rf, ok := ret.Get(1).(func(context.Context, typesv1beta2.LeaseID, string, uint, []string, io.Reader, io.Writer, io.Writer, bool, remotecommand.TerminalSizeQueue) error); ok {
 		r1 = rf(ctx, lID, service, podIndex, cmd, stdin, stdout, stderr, tty, tsq)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// ForwardedPortStatus provides a mock function with given fields: _a0, _a1
+func (_m *Client) ForwardedPortStatus(_a0 context.Context, _a1 typesv1beta2.LeaseID) (map[string][]v1beta2.ForwardedPortStatus, error) {
+	ret := _m.Called(_a0, _a1)
+
+	var r0 map[string][]v1beta2.ForwardedPortStatus
+	if rf, ok := ret.Get(0).(func(context.Context, typesv1beta2.LeaseID) map[string][]v1beta2.ForwardedPortStatus); ok {
+		r0 = rf(_a0, _a1)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(map[string][]v1beta2.ForwardedPortStatus)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, typesv1beta2.LeaseID) error); ok {
+		r1 = rf(_a0, _a1)
 	} else {
 		r1 = ret.Error(1)
 	}
@@ -282,15 +319,15 @@ func (_m *Client) LeaseLogs(_a0 context.Context, _a1 typesv1beta2.LeaseID, _a2 s
 }
 
 // LeaseStatus provides a mock function with given fields: _a0, _a1
-func (_m *Client) LeaseStatus(_a0 context.Context, _a1 typesv1beta2.LeaseID) (*v1beta2.LeaseStatus, error) {
+func (_m *Client) LeaseStatus(_a0 context.Context, _a1 typesv1beta2.LeaseID) (map[string]*v1beta2.ServiceStatus, error) {
 	ret := _m.Called(_a0, _a1)
 
-	var r0 *v1beta2.LeaseStatus
-	if rf, ok := ret.Get(0).(func(context.Context, typesv1beta2.LeaseID) *v1beta2.LeaseStatus); ok {
+	var r0 map[string]*v1beta2.ServiceStatus
+	if rf, ok := ret.Get(0).(func(context.Context, typesv1beta2.LeaseID) map[string]*v1beta2.ServiceStatus); ok {
 		r0 = rf(_a0, _a1)
 	} else {
 		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(*v1beta2.LeaseStatus)
+			r0 = ret.Get(0).(map[string]*v1beta2.ServiceStatus)
 		}
 	}
 
@@ -327,6 +364,29 @@ func (_m *Client) ObserveHostnameState(ctx context.Context) (<-chan v1beta2.Host
 	return r0, r1
 }
 
+// ObserveIPState provides a mock function with given fields: ctx
+func (_m *Client) ObserveIPState(ctx context.Context) (<-chan v1beta2.IPResourceEvent, error) {
+	ret := _m.Called(ctx)
+
+	var r0 <-chan v1beta2.IPResourceEvent
+	if rf, ok := ret.Get(0).(func(context.Context) <-chan v1beta2.IPResourceEvent); ok {
+		r0 = rf(ctx)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(<-chan v1beta2.IPResourceEvent)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context) error); ok {
+		r1 = rf(ctx)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // PurgeDeclaredHostname provides a mock function with given fields: ctx, lID, hostname
 func (_m *Client) PurgeDeclaredHostname(ctx context.Context, lID typesv1beta2.LeaseID, hostname string) error {
 	ret := _m.Called(ctx, lID, hostname)
@@ -343,6 +403,34 @@ func (_m *Client) PurgeDeclaredHostname(ctx context.Context, lID typesv1beta2.Le
 
 // PurgeDeclaredHostnames provides a mock function with given fields: ctx, lID
 func (_m *Client) PurgeDeclaredHostnames(ctx context.Context, lID typesv1beta2.LeaseID) error {
+	ret := _m.Called(ctx, lID)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, typesv1beta2.LeaseID) error); ok {
+		r0 = rf(ctx, lID)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// PurgeDeclaredIP provides a mock function with given fields: ctx, lID, serviceName, externalPort, proto
+func (_m *Client) PurgeDeclaredIP(ctx context.Context, lID typesv1beta2.LeaseID, serviceName string, externalPort uint32, proto v2beta1.ServiceProtocol) error {
+	ret := _m.Called(ctx, lID, serviceName, externalPort, proto)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, typesv1beta2.LeaseID, string, uint32, v2beta1.ServiceProtocol) error); ok {
+		r0 = rf(ctx, lID, serviceName, externalPort, proto)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// PurgeDeclaredIPs provides a mock function with given fields: ctx, lID
+func (_m *Client) PurgeDeclaredIPs(ctx context.Context, lID typesv1beta2.LeaseID) error {
 	ret := _m.Called(ctx, lID)
 
 	var r0 error

provider/cluster/service.go
@@ -2,6 +2,8 @@ package cluster
 
 import (
 	"context"
+	"github.com/ovrclk/akash/provider/cluster/operatorclients"
+	"github.com/ovrclk/akash/provider/operator/waiter"
 
 	"github.com/boz/go-lifecycle"
 	sdktypes "github.com/cosmos/cosmos-sdk/types"
@@ -59,7 +61,7 @@ type Service interface {
 }
 
 // NewService returns new Service instance
-func NewService(ctx context.Context, session session.Session, bus pubsub.Bus, client Client, cfg Config) (Service, error) {
+func NewService(ctx context.Context, session session.Session, bus pubsub.Bus, client Client, ipOperatorClient operatorclients.IPOperatorClient, waiter waiter.OperatorWaiter, cfg Config) (Service, error) {
 	log := session.Log().With("module", "provider-cluster", "cmp", "service")
 
 	lc := lifecycle.New()
@@ -75,7 +77,7 @@ func NewService(ctx context.Context, session session.Session, bus pubsub.Bus, cl
 		return nil, err
 	}
 
-	inventory, err := newInventoryService(cfg, log, lc.ShuttingDown(), sub, client, deployments)
+	inventory, err := newInventoryService(cfg, log, lc.ShuttingDown(), sub, client, ipOperatorClient, waiter, deployments)
 	if err != nil {
 		sub.Close()
 		return nil, err
@@ -114,10 +116,11 @@ func NewService(ctx context.Context, session session.Session, bus pubsub.Bus, cl
 		log:    log,
 		lc:     lc,
 		config: cfg,
+		waiter: waiter,
 	}
 
 	go s.lc.WatchContext(ctx)
-	go s.run(deployments)
+	go s.run(ctx, deployments)
 
 	return s, nil
 }
@@ -139,6 +142,8 @@ type service struct {
 
 	log log.Logger
 	lc  lifecycle.Lifecycle
+
+	waiter waiter.OperatorWaiter
 
 	config Config
 }
@@ -251,11 +256,19 @@ func (s *service) updateDeploymentManagerGauge() {
 	deploymentManagerGauge.Set(float64(len(s.managers)))
 }
 
-func (s *service) run(deployments []ctypes.Deployment) {
+func (s *service) run(ctx context.Context, deployments []ctypes.Deployment) {
 	defer s.lc.ShutdownCompleted()
 	defer s.sub.Close()
 
 	s.updateDeploymentManagerGauge()
+
+	// wait for configured operators to be online & responsive before proceeding
+	err := s.waiter.WaitForAll(ctx)
+	if err != nil {
+		s.lc.ShutdownInitiated(err)
+		return
+	}
+
 	for _, deployment := range deployments {
 		key := deployment.LeaseID()
 		mgroup := deployment.ManifestGroup()
@@ -272,7 +285,7 @@ loop:
 		case ev := <-s.sub.Events():
 			switch ev := ev.(type) {
 			case event.ManifestReceived:
-				s.log.Info("manifest received")
+				s.log.Info("manifest received", "lease", ev.LeaseID)
 
 				mgroup := ev.ManifestGroup()
 				if mgroup == nil {

provider/service.go
@@ -2,15 +2,15 @@ package provider
 
 import (
 	"context"
-	"time"
-
 	clustertypes "github.com/ovrclk/akash/provider/cluster/types/v1beta2"
+
+	"github.com/boz/go-lifecycle"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	bankTypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-
-	"github.com/boz/go-lifecycle"
+	"github.com/ovrclk/akash/provider/cluster/operatorclients"
+	"github.com/ovrclk/akash/provider/operator/waiter"
 	"github.com/pkg/errors"
 
 	"github.com/ovrclk/akash/provider/bidengine"
@@ -19,10 +19,6 @@ import (
 	"github.com/ovrclk/akash/provider/session"
 	"github.com/ovrclk/akash/pubsub"
 	dtypes "github.com/ovrclk/akash/x/deployment/types/v1beta2"
-)
-
-var (
-	ErrClusterReadTimedout = errors.New("timeout waiting for cluster ready")
 )
 
 // ValidateClient is the interface to check if provider will bid on given groupspec
@@ -56,7 +52,15 @@ type Service interface {
 // NewService creates and returns new Service instance
 // Simple wrapper around various services needed for running a provider.
 
-func NewService(ctx context.Context, cctx client.Context, accAddr sdk.AccAddress, session session.Session, bus pubsub.Bus, cclient cluster.Client, cfg Config) (Service, error) {
+func NewService(ctx context.Context,
+	cctx client.Context,
+	accAddr sdk.AccAddress,
+	session session.Session,
+	bus pubsub.Bus,
+	cclient cluster.Client,
+	ipOperatorClient operatorclients.IPOperatorClient,
+	waiter waiter.OperatorWaiter,
+	cfg Config) (Service, error) {
 	ctx, cancel := context.WithCancel(ctx)
 
 	session = session.ForModule("provider-service")
@@ -73,22 +77,13 @@ func NewService(ctx context.Context, cctx client.Context, accAddr sdk.AccAddress
 	clusterConfig.DeploymentIngressDomain = cfg.DeploymentIngressDomain
 	clusterConfig.ClusterSettings = cfg.ClusterSettings
 
-	cluster, err := cluster.NewService(ctx, session, bus, cclient, clusterConfig)
+	cluster, err := cluster.NewService(ctx, session, bus, cclient, ipOperatorClient, waiter, clusterConfig)
 	if err != nil {
 		cancel()
 		return nil, err
 	}
 
-	select {
-	case <-cluster.Ready():
-	case <-time.After(cfg.ClusterWaitReadyDuration):
-		session.Log().Error(ErrClusterReadTimedout.Error())
-		cancel()
-		<-cluster.Done()
-		return nil, ErrClusterReadTimedout
-	}
-
-	bidengine, err := bidengine.NewService(ctx, session, cluster, bus, bidengine.Config{
+	bidengine, err := bidengine.NewService(ctx, session, cluster, bus, waiter, bidengine.Config{
 		PricingStrategy: cfg.BidPricingStrategy,
 		Deposit:         cfg.BidDeposit,
 		BidTimeout:      cfg.BidTimeout,