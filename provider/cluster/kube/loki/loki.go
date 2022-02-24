92f6fc8a643e3876cf2389ce08c99ff9f27f3c69
Eric Urban <hydrogen18@gmail.com>
2022-02-24 12:54:02 -0600

    wip

---
+++ provider/cluster/kube/loki/loki.go
@@ -0,0 +1,15 @@
+package loki
+
+/*
+import (
+	loki "github.com/grafana/loki/pkg/logcli/client"
+)
+
+type client struct {
+	lc loki.Client
+}
+
+func (c *client) GetLogsByService(serviceName string) {
+
+}
+*/
