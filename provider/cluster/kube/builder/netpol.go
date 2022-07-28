29647ea8d392a62bf4b1f985bf23f87dd8969d92
Eric Urban <hydrogen18@gmail.com>
2022-07-28 12:18:55 -0500

    remove nil struct assignments

--- provider/cluster/kube/builder/netpol.go
+++ provider/cluster/kube/builder/netpol.go
@@ -213,12 +213,11 @@ func (b *netPol) Create() ([]*netv1.NetworkPolicy, error) { // nolint:golint,unp
 					Namespace: LidNS(b.lid),
 				},
 				Spec: netv1.NetworkPolicySpec{
+
 					Ingress: []netv1.NetworkPolicyIngressRule{
 						{
 							From: []netv1.NetworkPolicyPeer{
 								{
-									PodSelector:       nil,
-									NamespaceSelector: nil,
 									IPBlock: &netv1.IPBlock{
 										CIDR: "0.0.0.0/0",
 									},
@@ -232,6 +231,7 @@ func (b *netPol) Create() ([]*netv1.NetworkPolicy, error) { // nolint:golint,unp
 							AkashManifestServiceLabelName: serviceName,
 						},
 					},
+
 					PolicyTypes: []netv1.PolicyType{
 						netv1.PolicyTypeIngress,
 					},
