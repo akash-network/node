f1e378cd4c5845f4b925296a6326c4feffc96a9d
Artur Troian <troian.ap@gmail.com>
2022-09-29 13:13:23 -0400

    build(release): update goreleaser-cross v1.19.1
    release darwin universal binaries
    
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- make/releasing.mk
+++ make/releasing.mk
@@ -3,6 +3,15 @@ GORELEASER_DEBUG         ?= false
 GORELEASER_IMAGE         := ghcr.io/goreleaser/goreleaser-cross:v$(GOLANG_VERSION)
 GON_CONFIGFILE           ?= gon.json
 
+ifeq ($(GORELEASER_RELEASE),true)
+	GORELEASER_SKIP_VALIDATE := false
+	GORELEASER_SKIP_PUBLISH  := release --skip-publish=false
+else
+	GORELEASER_SKIP_PUBLISH  := --skip-publish=true
+	GORELEASER_SKIP_VALIDATE ?= false
+	GITHUB_TOKEN=
+endif
+
 ifeq ($(OS),Windows_NT)
     DETECTED_OS := Windows
 else
@@ -61,9 +70,8 @@ docker-image:
 gen-changelog: $(GIT_CHGLOG)
 	@echo "generating changelog to .cache/changelog"
 	./script/genchangelog.sh "$(RELEASE_TAG)" .cache/changelog.md
-
-.PHONY: release-dry-run
-release-dry-run: modvendor gen-changelog
+.PHONY: release
+release: modvendor gen-changelog
 	docker run \
 		--rm \
 		--privileged \
@@ -75,40 +83,16 @@ release-dry-run: modvendor gen-changelog
 		-e LINKMODE="$(GO_LINKMODE)" \
 		-e HOMEBREW_NAME="$(GORELEASER_HOMEBREW_NAME)" \
 		-e HOMEBREW_CUSTOM="$(GORELEASER_HOMEBREW_CUSTOM)" \
+		-e GITHUB_TOKEN="$(GITHUB_TOKEN)" \
+		-e GORELEASER_CURRENT_TAG="$(RELEASE_TAG)" \
 		-v /var/run/docker.sock:/var/run/docker.sock \
-		-v `pwd`:/go/src/github.com/ovrclk/akash \
-		-w /go/src/github.com/ovrclk/akash \
+		-v $(HOME)/.docker/config.json:/root/.docker/config.json \
+		-v `pwd`:/go/src/$(GO_MOD_NAME) \
+		-w /go/src/$(GO_MOD_NAME) \
 		$(GORELEASER_IMAGE) \
 		-f "$(GORELEASER_CONFIG)" \
+		$(GORELEASER_SKIP_PUBLISH) \
 		--skip-validate=$(GORELEASER_SKIP_VALIDATE) \
 		--debug=$(GORELEASER_DEBUG) \
 		--rm-dist \
-		--skip-publish \
-		--release-notes=/go/src/github.com/ovrclk/akash/.cache/changelog.md
-
-.PHONY: release
-release: modvendor gen-changelog
-	@if [ ! -f ".release-env" ]; then \
-		echo "\033[91m.release-env is required for release\033[0m";\
-		exit 1;\
-	fi
-	docker run \
-		--rm \
-		--privileged \
-		-e STABLE=$(IS_STABLE) \
-		-e MOD="$(GO_MOD)" \
-		-e BUILD_TAGS="$(BUILD_TAGS)" \
-		-e BUILD_VARS="$(GORELEASER_BUILD_VARS)" \
-		-e STRIP_FLAGS="$(GORELEASER_STRIP_FLAGS)" \
-		-e LINKMODE="$(GO_LINKMODE)" \
-		-e HOMEBREW_NAME="$(GORELEASER_HOMEBREW_NAME)" \
-		-e HOMEBREW_CUSTOM="$(GORELEASER_HOMEBREW_CUSTOM)" \
-		--env-file .release-env \
-		-v /var/run/docker.sock:/var/run/docker.sock \
-		-v `pwd`:/go/src/github.com/ovrclk/akash \
-		-w /go/src/github.com/ovrclk/akash \
-		$(GORELEASER_IMAGE) \
-		-f "$(GORELEASER_CONFIG)" release \
-		--debug=$(GORELEASER_DEBUG) \
-		--rm-dist \
-		--release-notes=/go/src/github.com/ovrclk/akash/.cache/changelog.md
+		--release-notes=/go/src/$(GO_MOD_NAME)/.cache/changelog.md
