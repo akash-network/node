626e55c950aad423e63ee483550b22e0099c3ec2
Artur Troian <troian.ap@gmail.com>
2023-03-04 09:22:59 -0500

    fix(app): add test coverage to FindStructField (#1769)
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

---
+++ app/types/app_test.go
@@ -0,0 +1,65 @@
+package types
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestFindStructFieldEmptyField(t *testing.T) {
+	type testType struct {
+		Val string
+	}
+	testStruct := testType{}
+
+	_, err := FindStructField[string](testStruct, "")
+	require.EqualError(t, err, ErrEmptyFieldName.Error())
+}
+
+func TestFindStructFieldObjectAsValue(t *testing.T) {
+	type testType struct {
+		Val string
+	}
+	testStruct := testType{}
+
+	val, err := FindStructField[string](testStruct, "Val")
+	require.NoError(t, err)
+	require.Equal(t, "", val)
+}
+
+func TestFindStructFieldObjectAsPointer(t *testing.T) {
+	type testType struct {
+		Val string
+	}
+	testStruct := testType{
+		Val: "testVal",
+	}
+
+	val, err := FindStructField[string](&testStruct, "Val")
+	require.NoError(t, err)
+	require.Equal(t, "testVal", val)
+}
+
+func TestFindStructFieldUnknownField(t *testing.T) {
+	type testType struct {
+		Val string
+	}
+	testStruct := testType{
+		Val: "testVal",
+	}
+
+	_, err := FindStructField[string](&testStruct, "Vals")
+	require.Error(t, err)
+}
+
+func TestFindStructFieldNonMatchingType(t *testing.T) {
+	type testType struct {
+		Val string
+	}
+	testStruct := testType{
+		Val: "testVal",
+	}
+
+	_, err := FindStructField[int](&testStruct, "Vals")
+	require.Error(t, err)
+}
