01761a9d6237b893550d529c05847286c15efb37
Artur Troian <troian.ap@gmail.com>
2023-06-15 11:48:19 -0400

    build: remove need for vendor (#1839)
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- make/environment.mk
+++
@@ -1,5 +0,0 @@
-.PHONY: setup-devenv
-setup-devenv: $(PROTOC) $(GRPC_GATEWAY) $(MODVENDOR) $(PROTOC_SWAGGER_GEN) $(KIND) deps-vendor modvendor
-
-.PHONY: setup-cienv
-setup-cienv: deps-vendor modvendor
