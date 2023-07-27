8494a3161fe89e5fd577f881be0d269233b9c92e
Artur Troian <troian.ap@gmail.com>
2023-07-27 10:01:09 -0400

    feat(tools): add upgrade info generator
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

---
+++ tools/upgrade-info/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	utilcli "github.com/akash-network/node/util/cli"
+)
+
+func main() {
+	cmd := cobra.Command{
+		Use:     "upgrade-info",
+		Example: "upgrade-info <tag> <file>",
+		Args:    cobra.RangeArgs(1, 2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			info, err := utilcli.UpgradeInfoFromTag(cmd.Context(), args[0], true)
+			if err != nil {
+				return err
+			}
+
+			if len(args) == 1 {
+				fmt.Printf("%s\n", info)
+				return nil
+			}
+
+			file, err := os.Create(args[1])
+			if err != nil {
+				return err
+			}
+			defer func() {
+				_ = file.Close()
+			}()
+
+			if _, err = file.WriteString(info); err != nil {
+				return err
+			}
+			return nil
+		},
+	}
+	if err := cmd.ExecuteContext(context.Background()); err != nil {
+		log.Fatal(err)
+	}
+}
