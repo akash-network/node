29e52f5932a934dfc437fdf7431225c7c83954e1
Artur Troian <troian.ap@gmail.com>
2023-08-03 14:23:58 -0400

    refactor(v1beta3): convert params from map to slice
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- x/deployment/simulation/genesis.go
+++ x/deployment/simulation/genesis.go
@@ -1,6 +1,7 @@
 package simulation
 
 import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 
 	types "github.com/akash-network/akash-api/go/node/deployment/v1beta3"
@@ -14,8 +15,8 @@ var (
 func RandomizedGenState(simState *module.SimulationState) {
 	deploymentGenesis := &types.GenesisState{
 		Params: types.Params{
-			MinDeposits: map[string]uint32{
-				minDeposit.Denom: uint32(minDeposit.Amount.ToDec().TruncateInt64()),
+			MinDeposits: sdk.Coins{
+				minDeposit,
 			},
 		},
 	}
