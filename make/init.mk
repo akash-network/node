e2b4fe11a0cd5d51c68af3134c52adab4cf87543
Artur Troian <troian.ap@gmail.com>
2023-08-10 13:53:48 -0400

    feat: use gotoolchain
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- make/init.mk
+++ make/init.mk
@@ -21,40 +21,30 @@ ifndef AKASH_ROOT
 	PATH                := $(AKASH_DEVCACHE_BIN):$(AKASH_DEVCACHE_NODE_BIN):$(PATH)
 endif
 
-SEMVER                       := $(ROOT_DIR)/script/semver.sh
-
-# require go<major>.<minor> to be equal
-GO_MIN_REQUIRED              := $(shell echo $(GOLANG_VERSION) | cut -f1-2 -d ".")
-DETECTED_GO_VERSION          := $(shell go version | cut -d ' ' -f 3 |  sed 's/go*//')
-STRIPPED_GO_VERSION          := $(shell echo $(DETECTED_GO_VERSION) | cut -f1-2 -d ".")
-__IS_GO_UPTODATE             := $(shell $(ROOT_DIR)/script/semver.sh compare $(STRIPPED_GO_VERSION) $(GO_MIN_REQUIRED); echo $?)
-GO_MOD_VERSION               := $(shell go mod edit -json | jq -r .Go | cut -f1-2 -d ".")
-__IS_GO_MOD_MATCHING         := $(shell $(SEMVER) compare $(GO_MOD_VERSION) $(GO_MIN_REQUIRED) && echo $?)
-
-ifneq (0, $(__IS_GO_MOD_MATCHING))
-$(error go version $(GO_MOD_VERSION) from go.mod does not match GO_MIN_REQUIRED=$(GO_MIN_REQUIRED))
-endif
-
-ifneq (0, $(__IS_GO_UPTODATE))
-$(error invalid go$(DETECTED_GO_VERSION) version. installed must be == $(GO_MIN_REQUIRED))
-else
-$(info using go$(DETECTED_GO_VERSION))
-endif
-
 BINS                         := $(AKASH)
+SEMVER                       := $(AKASH_ROOT)/script/semver.sh
 
+__local_go                   := $(shell GOTOOLCHAIN=local go version | cut -d ' ' -f 3 | sed 's/go*//' | tr -d '\n')
+__is_local_go_satisfies      := $(shell $(SEMVER) compare "v$(__local_go)" "v1.20.7"; echo $?)
+
+ifeq (-1, $(__is_local_go_satisfies))
+$(error "unsupported local go$(__local_go) version . min required go1.21.0")
+endif
+
+GO_VERSION                   := $(shell go mod edit -json | jq -r .Go | tr -d '\n')
+GOTOOLCHAIN                  := $(shell go mod edit -json | jq -r .Toolchain | tr -d '\n')
+GOTOOLCHAIN_SEMVER           := v$(shell echo "$(GOTOOLCHAIN)" | sed 's/go*//' | tr -d '\n')
+
+GO                           := GO111MODULE=$(GO111MODULE) go
 GOWORK                       ?= on
-
 GO_MOD                       ?= vendor
 ifeq ($(GOWORK), on)
 GO_MOD                       := readonly
 endif
 
-GO                           := GO111MODULE=$(GO111MODULE) go
 GO_BUILD                     := $(GO) build -mod=$(GO_MOD)
 GO_TEST                      := $(GO) test -mod=$(GO_MOD)
 GO_VET                       := $(GO) vet -mod=$(GO_MOD)
-
 GO_MOD_NAME                  := $(shell go list -m 2>/dev/null)
 
 ifeq ($(OS),Windows_NT)
@@ -63,13 +53,13 @@ else
 	DETECTED_OS := $(shell sh -c 'uname 2>/dev/null || echo Unknown')
 endif
 
-# on MacOS disable deprecation warnings security framework
 ifeq ($(DETECTED_OS), Darwin)
+	# on MacOS disable deprecation warnings security framework
 	export CGO_CFLAGS=-Wno-deprecated-declarations
 
 	# on MacOS Sonoma Beta there is a bit of discrepancy between Go and new prime linker
 	clang_version := $(shell echo | clang -dM -E - | grep __clang_major__ | cut -d ' ' -f 3 | tr -d '\n')
-	go_has_ld_fix := $(shell $(SEMVER) compare "v$(DETECTED_GO_VERSION)" "v1.21.0" | tr -d '\n')
+	go_has_ld_fix := $(shell $(SEMVER) compare "v$(GOTOOLCHAIN_SEMVER)" "v1.22.0" | tr -d '\n')
 
 	ifeq (15,$(clang_version))
 		ifeq (-1,$(go_has_ld_fix))
@@ -81,11 +71,10 @@ endif
 # ==== Build tools versions ====
 # Format <TOOL>_VERSION
 GOLANGCI_LINT_VERSION        ?= v1.51.2
-GOLANG_VERSION               ?= 1.16.1
 STATIK_VERSION               ?= v0.1.7
 GIT_CHGLOG_VERSION           ?= v0.15.1
 MOCKERY_VERSION              ?= 2.24.0
-COSMOVISOR_VERSION           ?= v1.4.0
+COSMOVISOR_VERSION           ?= v1.5.0
 
 # ==== Build tools version tracking ====
 # <TOOL>_VERSION_FILE points to the marker file for the installed version.
