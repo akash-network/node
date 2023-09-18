a563df9442bc78ff2b3ff3878f81124fd2eea509
Artur Troian <troian.ap@gmail.com>
2023-09-18 13:56:27 -0400

    Revert "chore(tests): wait a block period prior quering for upgrade proposal (#1894)"
    This reverts commit 9dfbfd8209533c2597b46d49a830cb387ee0533b.

--- tests/upgrade/upgrade_test.go
+++ tests/upgrade/upgrade_test.go
@@ -6,7 +6,6 @@ import (
 	"bufio"
 	"context"
 	"encoding/json"
-	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -696,14 +695,6 @@ func (l *upgradeTest) submitUpgradeProposal() error {
 	cmdRes, err = l.cmdr.execute(l.ctx, cmd)
 	if err != nil {
 		l.t.Logf("executing cmd failed: %s\n", string(cmdRes))
-		return err
-	}
-
-	timeout, tCancel := context.WithTimeout(l.ctx, 7*time.Second)
-	defer tCancel()
-
-	<-timeout.Done()
-	if err := timeout.Err(); err != nil && !errors.Is(err, context.DeadlineExceeded) {
 		return err
 	}
 
