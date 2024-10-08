cf2ef622c0621f0e483021f883bc6cb61c9d0f7c
Artur Troian <troian.ap@gmail.com>
2024-10-08 09:53:09 -0500

    ci: reuse workflows
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- script/tools.sh
+++ script/tools.sh
@@ -5,6 +5,30 @@ SEMVER=$SCRIPT_DIR/semver.sh
 
 gomod="$SCRIPT_DIR/../go.mod"
 
+macos_deps=(
+	"bash"
+	"direnv"
+	"pv"
+	"lz4"
+	"git-cliff"
+)
+
+debian_deps=(
+	"make"
+	"build-essentials"
+	"direnv"
+	"unzip"
+	"wget"
+	"curl"
+	"npm"
+	"jq"
+	"coreutils"
+)
+
+is_command() {
+	command -v "$1" >/dev/null
+}
+
 function get_gotoolchain() {
 	local gotoolchain
 	local goversion
@@ -115,6 +139,80 @@ function replace_import_path() {
 	done
 
 	replace_paths "./go/go.mod" "$curr_module_name" "$new_module_name"
+}
+
+function install_gha_deps() {
+	if [[ "$OSTYPE" == "darwin"* ]]; then
+		echo "Detected Darwin based system"
+
+		if ! is_command brew; then
+			echo "homebrew is not installed. visit https://brew.sh"
+			exit 1
+		fi
+
+		local tools
+
+		if ! is_command make || [[ $(make --version | head -1 | cut -d" " -f3 | cut -d"." -f1) -lt 4 ]]; then
+			tools="$tools make"
+		fi
+
+		# shellcheck disable=SC2068
+		for dep in ${macos_deps[@]}; do
+			echo -n "detecting $dep ..."
+			status="(installed)"
+			if ! brew list "$dep" >/dev/null 2>&1; then
+				tools="$tools $dep"
+				status="(not installed)"
+			fi
+
+			echo " $status"
+		done
+
+		if [[ "$tools" != "" ]]; then
+			# don't put quotes around $tools!
+			# shellcheck disable=SC2086
+			brew install $tools
+		else
+			echo "All requirements already met. Nothing to install"
+		fi
+	elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+		if is_command dpkg; then
+			echo "Detected Debian based system"
+			local tools
+
+			# shellcheck disable=SC2068
+			for dep in ${debian_deps[@]}; do
+				echo -n "detecting $dep ..."
+				status="(installed)"
+				if ! dpkg -l "$dep"; then
+					tools="$tools $dep"
+					status="(not installed)"
+				fi
+				echo " $status"
+			done
+
+			cmd="apt-get"
+
+			if is_command sudo; then
+				cmd="sudo $cmd"
+			fi
+
+			if [[ "$tools" != "" ]]; then
+				$cmd update
+				# don't put quotes around $tools!
+				# shellcheck disable=SC2086
+				(
+					set -x
+					$cmd install -y $tools
+				)
+			else
+				echo "All requirements already met. Nothing to install"
+			fi
+		fi
+	else
+		echo "Unsupported OS $OSTYPE"
+		exit 1
+	fi
 }
 
 case "$1" in
