cce8bffc67e33340177310db5c9a10249a39eec5
Joseph Chalabi <chalabi.joseph@gmail.com>
2026-05-19 18:19:32 -0700

    fix(verification): delegate interface registration
    Signed-off-by: Joseph Chalabi <chalabi.joseph@gmail.com>

--- x/verification/types/codec.go
+++ x/verification/types/codec.go
@@ -3,8 +3,12 @@ package types
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
+
+	vtypes "pkg.akt.dev/go/node/verification/v1"
 )
 
 func RegisterLegacyAminoCodec(_ *codec.LegacyAmino) {}
 
-func RegisterInterfaces(_ cdctypes.InterfaceRegistry) {}
+func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
+	vtypes.RegisterInterfaces(registry)
+}
